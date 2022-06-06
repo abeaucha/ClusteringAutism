63ed19be9e8c65811b91a4ae710c40802b69cc91
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-06-06 10:15:19 -0400

    Created script templates

---
+++ template.R
@@ -0,0 +1,50 @@
+# ----------------------------------------------------------------------------
+# template.R
+# Author: Antoine Beauchamp
+# Created:
+#
+# Brief description
+#
+# Description
+# -----------
+
+# Packages -------------------------------------------------------------------
+
+suppressPackageStartupMessages(library(optparse))
+
+
+# Command line arguments -----------------------------------------------------
+
+option_list <- list(
+  make_option('--arg',
+              type = 'character',
+              help = paste("Help message")),
+  make_option('--verbose',
+              type = 'character',
+              default = 'true',
+              help = paste("Verbosity option. [default %default]"))
+) 
+
+
+# Functions ------------------------------------------------------------------
+
+#' Function description
+#'
+#' @param x (class) Parameter description.
+#'
+#' @return (class) Return description.
+template <- function(x){
+  return()
+}
+
+
+# Main -----------------------------------------------------------------------
+
+#Parse command line args
+args <- parse_args(OptionParser(option_list = option_list))
+arg <- args[['arg']]
+verbose <- ifelse(args[['verbose']] == 'true', TRUE, FALSE)
+
+#Import cluster information
+if (verbose) {message("")}
+
---
+++ template.py
@@ -0,0 +1,83 @@
+# ----------------------------------------------------------------------------
+# template.py 
+# Author: Antoine Beauchamp
+# Created: 
+
+"""
+Brief description.
+
+Description
+-----------
+"""
+
+# Packages -------------------------------------------------------------------
+
+import argparse
+import os
+
+
+# Command line arguments -----------------------------------------------------
+
+def parse_args():
+    
+    """Parse command line arguments"""
+    
+    parser = argparse.ArgumentParser(
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    )
+    
+    parser.add_argument(
+        '--arg',
+        type = str,
+        help = ("Help message")
+    )
+    
+    parser.add_argument(
+        '--verbose',
+        type = str,
+        default = 'true',
+        choices = ['true', 'false'],
+        help = 'Verbosity.'
+    )
+    
+    args = vars(parser.parse_args())
+    
+    return args
+
+
+# Functions ------------------------------------------------------------------
+
+def template(x):
+    
+    """
+    Template
+    
+    Arguments
+    ---------
+    x: dtype
+        Argument description.
+        
+    Returns
+    -------
+    None
+    """
+    
+    return
+    
+
+# Main -----------------------------------------------------------------------
+
+def main():
+    
+    #Parse command line arguments
+    args = parse_args()
+    arg = args['arg1']
+    verbose = True if args['verbose'] == 'true' else False
+    
+    if verbose:
+        print("")
+    
+    return
+    
+if __name__=='__main__':
+    main()
