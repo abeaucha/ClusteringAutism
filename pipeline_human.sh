40e0aa60bb7e92f7446a6a90107fa36eb4df5103
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-06-23 12:00:38 -0400

    Removed median effect size commands

--- pipeline_human.sh
+++ pipeline_human.sh
@@ -13,9 +13,8 @@ source activate_venv.sh
 #	--parallel true \
 #	--nproc 8
 
-echo "Calculating human effect sizes using absolute jacobians..."
-
 #Calculate human effect sizes based on relative and absolute volumes
+echo "Calculating human effect sizes using absolute jacobians..."
 # Rscript calculate_human_effect_sizes.R \
 # 	--demographics data/human/registration/DBM_input_demo_passedqc.csv \
 # 	--imgdir data/human/registration/jacobians/absolute/smooth/ \
@@ -26,7 +25,6 @@ echo "Calculating human effect sizes using absolute jacobians..."
 # 	--nproc 4
 
 echo "Calculating human effect sizes using relative jacobians..."
-
 # Rscript calculate_human_effect_sizes.R \
 # 	--demographics data/human/registration/DBM_input_demo_passedqc.csv \
 # 	--imgdir data/human/registration/jacobians/relative/smooth/ \
@@ -48,13 +46,13 @@ echo "Resampling template and mask images..."
 # 	data/human/registration/reference_files/mask_3.0mm.mnc
     
 #Downsample model and mask files to 1.0mm
-autocrop -quiet -clobber -isostep 1.0 \
-	data/human/registration/reference_files/model.mnc \
-	data/human/registration/reference_files/model_1.0mm.mnc
+# autocrop -quiet -clobber -isostep 1.0 \
+# 	data/human/registration/reference_files/model.mnc \
+# 	data/human/registration/reference_files/model_1.0mm.mnc
 
-autocrop -quiet -clobber -isostep 1.0 \
-	data/human/registration/reference_files/mask.mnc \
-	data/human/registration/reference_files/mask_1.0mm.mnc
+# autocrop -quiet -clobber -isostep 1.0 \
+# 	data/human/registration/reference_files/mask.mnc \
+# 	data/human/registration/reference_files/mask_1.0mm.mnc
     
 echo "Resampling absolute effect size images..."
 
@@ -67,12 +65,12 @@ echo "Resampling absolute effect size images..."
 # 	--nproc 4
 
 #Downsample human effect size images to 1.0mm
-python3 resample_minc_images.py \
-	--imgdir data/human/effect_sizes/absolute/resolution_0.5/ \
-	--outdir data/human/effect_sizes/absolute/resolution_1.0/ \
-	--isostep 1.0 \
-	--parallel true \
-	--nproc 4
+# python3 resample_minc_images.py \
+# 	--imgdir data/human/effect_sizes/absolute/resolution_0.5/ \
+# 	--outdir data/human/effect_sizes/absolute/resolution_1.0/ \
+# 	--isostep 1.0 \
+# 	--parallel true \
+# 	--nproc 4
     
 echo "Resampling relative effect size images..."
 
@@ -85,12 +83,12 @@ echo "Resampling relative effect size images..."
 # 	--nproc 4
 
 #Downsample human effect size images to 1.0mm
-python3 resample_minc_images.py \
-	--imgdir data/human/effect_sizes/relative/resolution_0.5/ \
-	--outdir data/human/effect_sizes/relative/resolution_1.0/ \
-	--isostep 1.0 \
-	--parallel true \
-	--nproc 4
+# python3 resample_minc_images.py \
+# 	--imgdir data/human/effect_sizes/relative/resolution_0.5/ \
+# 	--outdir data/human/effect_sizes/relative/resolution_1.0/ \
+# 	--isostep 1.0 \
+# 	--parallel true \
+# 	--nproc 4
     
 echo "Build absolute effect size matrix..."
 
@@ -101,10 +99,10 @@ echo "Build absolute effect size matrix..."
 # 	--mask data/human/registration/reference_files/mask_3.0mm.mnc \
     
 #Create matrices for human effect sizes at 1.0mm
-python3 build_effect_size_matrix.py \
-	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
-	--outfile data/human/effect_sizes/absolute/human_effect_sizes_absolute_1.0mm.csv \
-	--mask data/human/registration/reference_files/mask_1.0mm.mnc \
+# python3 build_effect_size_matrix.py \
+# 	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
+# 	--outfile data/human/effect_sizes/absolute/human_effect_sizes_absolute_1.0mm.csv \
+# 	--mask data/human/registration/reference_files/mask_1.0mm.mnc \
     
 echo "Build relative effect size matrix..."    
 
@@ -113,10 +111,10 @@ echo "Build relative effect size matrix..."
 # 	--outfile data/human/effect_sizes/relative/human_effect_sizes_relative_3.0mm.csv \
 # 	--mask data/human/registration/reference_files/mask_3.0mm.mnc \
 
-python3 build_effect_size_matrix.py \
-	--imgdir data/human/effect_sizes/relative/resolution_1.0/ \
-	--outfile data/human/effect_sizes/relative/human_effect_sizes_relative_1.0mm.csv \
-	--mask data/human/registration/reference_files/mask_1.0mm.mnc \
+# python3 build_effect_size_matrix.py \
+# 	--imgdir data/human/effect_sizes/relative/resolution_1.0/ \
+# 	--outfile data/human/effect_sizes/relative/human_effect_sizes_relative_1.0mm.csv \
+# 	--mask data/human/registration/reference_files/mask_1.0mm.mnc \
     
 echo "Clustering human effect size images..."
     
@@ -132,17 +130,6 @@ echo "Clustering human effect size images..."
 # 	--metric cor \
 # 	--outfile data/human/clustering/human_clusters_groups10_3.0mm.csv
 
-Rscript cluster_human_data.R \
-	--file1 data/human/effect_sizes/absolute/human_effect_sizes_absolute_1.0mm.csv \
-	--file2 data/human/effect_sizes/relative/human_effect_sizes_relative_1.0mm.csv \
-	--rownames file \
-	--nclusters 10 \
-	--K 10 \
-	--t 20 \
-	--sigma 0.5 \
-	--metric cor \
-	--outfile data/human/clustering/human_clusters_groups10_1.0mm.csv
-    
 echo "Creating cluster maps using absolute effect sizes and mean aggregation..."
 
 # Create cluster maps for abs and rel volumes using mean and median
@@ -153,29 +140,13 @@ echo "Creating cluster maps using absolute effect sizes and mean aggregation..."
 # 	--method mean \
 # 	--jacobians absolute
     
-Rscript create_cluster_maps.R \
-	--clusterfile data/human/clustering/human_clusters_groups10_1.0mm.csv \
-	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
-	--outdir data/human/clustering/cluster_maps/absolute/resolution_1.0/mean/ \
-	--method mean \
-	--jacobians absolute
-
-echo "Creating cluster maps using absolute effect sizes and median aggregation..."
-    
 # Rscript create_cluster_maps.R \
 # 	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
-# 	--imgdir data/human/effect_sizes/absolute/resolution_3.0/ \
-# 	--outdir data/human/clustering/cluster_maps/absolute/resolution_3.0/median/ \
-# 	--method median \
+# 	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
+# 	--outdir data/human/clustering/cluster_maps/absolute/resolution_1.0/mean/ \
+# 	--method mean \
 # 	--jacobians absolute
 
-# Rscript create_cluster_maps.R \
-# 	--clusterfile data/human/clustering/human_clusters_groups10_1.0mm.csv \
-# 	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
-# 	--outdir data/human/clustering/cluster_maps/absolute/resolution_1.0/median/ \
-# 	--method median \
-# 	--jacobians absolute
-    
 echo "Creating cluster maps using relative effect sizes and mean aggregation..."
 
 # Rscript create_cluster_maps.R \
@@ -186,19 +157,10 @@ echo "Creating cluster maps using relative effect sizes and mean aggregation..."
 # 	--jacobians relative
     
 Rscript create_cluster_maps.R \
-	--clusterfile data/human/clustering/human_clusters_groups10_1.0mm.csv \
+	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
 	--imgdir data/human/effect_sizes/relative/resolution_1.0/ \
 	--outdir data/human/clustering/cluster_maps/relative/resolution_1.0/mean/ \
 	--method mean \
 	--jacobians relative
     
-echo "Creating cluster maps using relative effect sizes and median aggregation..."
-
-# Rscript create_cluster_maps.R \
-# 	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
-# 	--imgdir data/human/effect_sizes/relative/resolution_3.0/ \
-# 	--outdir data/human/clustering/cluster_maps/relative/resolution_3.0/median/ \
-# 	--method median \
-# 	--jacobians relative
-
 deactivate
