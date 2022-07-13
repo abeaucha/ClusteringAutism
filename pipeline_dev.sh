9f54e34cd5a3e5e839c3969f510bb9735dc1f8d3
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-13 15:31:25 -0400

    Create cluster masks, transform microarray samples, and create cluster signatures

--- pipeline_dev.sh
+++ pipeline_dev.sh
@@ -2,174 +2,153 @@
 
 source activate_venv.sh
 
-# echo "Organizing gene expression files..."
-# source organize_expression_files.sh
+# Get mouse clustering files from Jacob's directories
+# source get_mouse_files.sh
 
-# Rscript fetch_microarray_coordinates.R \
-#     --metadata data/human/SampleInformation_pipeline_v1.csv \
-#     --outfile data/human/expression/AHBA_microarray_coordinates_mni.csv \
-#     --labels true
 
-# antsApplyTransformsToPoints \
-#     -d 3 \
-#     --input data/human/expression/AHBA_microarray_coordinates_mni.csv \
-#     --output data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
-#     -t [data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc0_GenericAffine.xfm,1] \
-# 	-t data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc1_inverse_NL.xfm \
-#     --precision 1
+# Generate mouse and human cluster masks -------------------------------------
+
+# Create cluster masks using multiple thresholds
+thresholds=(0.1 0.2 0.3 0.4 0.5 0.6 0.8 0.7 0.9 1.0)
+for i in ${thresholds[@]};
+do
+    echo "Creating masks at threshold ${i} ..."
+
+    # Create mouse mask using absolute effect sizes at 200um
+    python3 create_cluster_masks.py \
+        --imgdir data/mouse/clustering/cluster_maps/absolute/resolution_200/mean/ \
+        --outdir data/mouse/clustering/cluster_masks/absolute/resolution_200/mean/threshold_$i/ \
+        --threshold $i \
+        --symmetric true
+        
+    # Create mouse mask using relative effect sizes at 200um    
+    python3 create_cluster_masks.py \
+        --imgdir data/mouse/clustering/cluster_maps/relative/resolution_200/mean/ \
+        --outdir data/mouse/clustering/cluster_masks/relative/resolution_200/mean/threshold_$i/ \
+        --threshold $i \
+        --symmetric true
+
+    # Create human mask using absolute effect sizes at 3.0mm
+    python3 create_cluster_masks.py \
+        --imgdir data/human/clustering/cluster_maps/absolute/resolution_3.0/mean/ \
+        --outdir data/human/clustering/cluster_masks/absolute/resolution_3.0/mean/threshold_$i/ \
+        --threshold $i \
+        --symmetric true
+
+    # Create human mask using relative effect sizes at 3.0mm        
+    python3 create_cluster_masks.py \
+        --imgdir data/human/clustering/cluster_maps/relative/resolution_3.0/mean/ \
+        --outdir data/human/clustering/cluster_masks/relative/resolution_3.0/mean/threshold_$i/ \
+        --threshold $i \
+        --symmetric true
+        
+    # Create human mask using absolute effect sizes at 1.0mm
+    python3 create_cluster_masks.py \
+        --imgdir data/human/clustering/cluster_maps/absolute/resolution_1.0/mean/ \
+        --outdir data/human/clustering/cluster_masks/absolute/resolution_1.0/mean/threshold_$i/ \
+        --threshold $i \
+        --symmetric true
+
+    # Create human mask using relative effect sizes at 1.0mm
+    python3 create_cluster_masks.py \
+        --imgdir data/human/clustering/cluster_maps/relative/resolution_1.0/mean/ \
+        --outdir data/human/clustering/cluster_masks/relative/resolution_1.0/mean/threshold_$i/ \
+        --threshold $i \
+        --symmetric true
+
+done
+
+
+# Re-organize gene expression files ------------------------------------------
+
+echo "Organizing gene expression files..."
+source organize_expression_files.sh
+
+
+# Transform human microarray samples -----------------------------------------
+
+#Download microarray sample coordinates in MNI space
+Rscript fetch_microarray_coordinates.R \
+    --metadata data/human/SampleInformation_pipeline_v1.csv \
+    --outfile data/human/expression/AHBA_microarray_coordinates_mni.csv \
+    --labels true
     
-# Rscript coordinates_to_minc.R \
-#     --coordinates data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
-#     --template data/human/registration/reference_files/model_1.0mm.mnc \
-#     --outfile data/human/expression/AHBA_microarray_mask_studyspace.mnc \
-#     --type mask
+#Transform microarray sample coordinates from MNI space to study space
+#Coordinates need to be transformed using inverse transforms
+antsApplyTransformsToPoints \
+   -d 3 \
+   --input data/human/expression/AHBA_microarray_coordinates_mni.csv \
+   --output data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
+   -t [data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc0_GenericAffine.xfm,0] \
+   -t data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc1_NL.xfm
+   
+# Create a mask indicating sample positions
+Rscript coordinates_to_minc.R \
+    --coordinates data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
+    --template data/human/registration/reference_files/model_1.0mm.mnc \
+    --outfile data/human/expression/AHBA_microarray_mask_studyspace_1.0mm.mnc \
+    --type mask
+    
+# Create a pseudo-atlas for individual samples
+Rscript coordinates_to_minc.R \
+    --coordinates data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
+    --template data/human/registration/reference_files/model_1.0mm.mnc \
+    --outfile data/human/expression/AHBA_microarray_labels_studyspace_1.0mm.mnc \
+    --type labels
+    
 
-Rscript human_cluster_signatures_dev.R \
-    --clusterdir data/human/clustering/cluster_masks/absolute/resolution_1.0/mean/threshold_0.1/ \
+# Generate mouse and human cluster signatures using input gene space ---------
+
+# Generate cluster signatures using multiple thresholds
+thresholds=(0.1 0.2 0.3 0.4 0.5 0.6 0.8 0.7 0.9 1.0)
+for i in ${thresholds[@]};
+do
+    echo "Creating mouse cluster signatures using threshold $i masks and input gene space..."
+    
+    # Mouse cluster signatures using absolute masks at 200um
+    python3 mouse_cluster_signatures.py \
+        --clusterdir data/mouse/clustering/cluster_masks/absolute/resolution_200/mean/threshold_$i/ \
+        --exprdir data/mouse/expression/input_space/ \
+        --mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
+        --outfile data/mouse/cluster_signatures/input_space/mouse_cluster_signatures_abs_mean_threshold${i}_inputspace.csv \
+        --parallel true \
+        --nproc 4
+
+    # Mouse cluster signatures using relative masks at 200um        
+    python3 mouse_cluster_signatures.py \
+        --clusterdir data/mouse/clustering/cluster_masks/relative/resolution_200/mean/threshold_$i/ \
+        --exprdir data/mouse/expression/input_space/ \
+        --mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
+        --outfile data/mouse/cluster_signatures/input_space/mouse_cluster_signatures_rel_mean_threshold${i}_inputspace.csv \
+        --parallel true \
+        --nproc 4
+        
+    echo "Creating human cluster signatures using threshold $i masks and input gene space..."
+
+    # Human cluster signatures using absolute masks at 1.0mm
+    Rscript human_cluster_signatures.R \
+    --clusterdir data/human/clustering/cluster_masks/absolute/resolution_1.0/mean/threshold_$i/ \
     --exprdir data/human/expression/input_space/ \
     --coordinates data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
     --template data/human/registration/reference_files/model_1.0mm.mnc \
-    --outfile data/human/cluster_signatures/input_space/human_cluster_signatures_rel_mean_threshold0.1_inputspace.csv \
+    --outfile data/human/cluster_signatures/input_space/human_cluster_signatures_abs_mean_threshold${i}_inputspace.csv \
     --parallel true \
     --nproc 4
-
-# python3 human_cluster_signatures_dev.py
-
-
-#Create cluster masks
-# thresholds=(0.1 0.2 0.3 0.4 0.5 0.6 0.8 0.7 0.9 1.0)
-# for i in ${thresholds[@]};
-# do
-#     echo "Creating masks at threshold ${i} ..."
-
-#     python3 create_cluster_masks.py \
-#         --imgdir data/mouse/clustering/cluster_maps/absolute/resolution_200/mean/ \
-#         --outdir data/mouse/clustering/cluster_masks/absolute/resolution_200/mean/threshold_$i/ \
-#         --threshold $i \
-#         --symmetric true
-        
-#     python3 create_cluster_masks.py \
-#         --imgdir data/mouse/clustering/cluster_maps/relative/resolution_200/mean/ \
-#         --outdir data/mouse/clustering/cluster_masks/relative/resolution_200/mean/threshold_$i/ \
-#         --threshold $i \
-#         --symmetric true
-        
-#     python3 create_cluster_masks.py \
-#         --imgdir data/human/clustering/cluster_maps/absolute/resolution_3.0/mean/ \
-#         --outdir data/human/clustering/cluster_masks/absolute/resolution_3.0/mean/threshold_$i/ \
-#         --threshold $i \
-#         --symmetric true
-        
-#     python3 create_cluster_masks.py \
-#         --imgdir data/human/clustering/cluster_maps/relative/resolution_3.0/mean/ \
-#         --outdir data/human/clustering/cluster_masks/relative/resolution_3.0/mean/threshold_$i/ \
-#         --threshold $i \
-#         --symmetric true
-        
-#     python3 create_cluster_masks.py \
-#         --imgdir data/human/clustering/cluster_maps/absolute/resolution_1.0/mean/ \
-#         --outdir data/human/clustering/cluster_masks/absolute/resolution_1.0/mean/threshold_$i/ \
-#         --threshold $i \
-#         --symmetric true
-        
-#     python3 create_cluster_masks.py \
-#         --imgdir data/human/clustering/cluster_maps/relative/resolution_1.0/mean/ \
-#         --outdir data/human/clustering/cluster_masks/relative/resolution_1.0/mean/threshold_$i/ \
-#         --threshold $i \
-#         --symmetric true
-
-# done
-
-
-# for i in ${thresholds[@]};
-# do
-#     echo "Creating mouse cluster signatures using threshold $i masks and input gene space..."
     
-#     python3 mouse_cluster_signatures.py \
-#         --clusterdir data/mouse/clustering/cluster_masks/absolute/resolution_200/mean/threshold_$i/ \
-#         --exprdir data/mouse/expression/input_space/ \
-#         --mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
-#         --outfile data/mouse/cluster_signatures/input_space/mouse_cluster_signatures_abs_mean_threshold${i}_inputspace.csv \
-#         --parallel true \
-#         --nproc 4
+    # Human cluster signatures using relative masks at 1.0mm    
+    Rscript human_cluster_signatures.R \
+    --clusterdir data/human/clustering/cluster_masks/relative/resolution_1.0/mean/threshold_$i/ \
+    --exprdir data/human/expression/input_space/ \
+    --coordinates data/human/expression/AHBA_microarray_coordinates_studyspace.csv \
+    --template data/human/registration/reference_files/model_1.0mm.mnc \
+    --outfile data/human/cluster_signatures/input_space/human_cluster_signatures_rel_mean_threshold${i}_inputspace.csv \
+    --parallel true \
+    --nproc 4
         
-#     python3 mouse_cluster_signatures.py \
-#         --clusterdir data/mouse/clustering/cluster_masks/relative/resolution_200/mean/threshold_$i/ \
-#         --exprdir data/mouse/expression/input_space/ \
-#         --mask data/mouse/atlas/coronal_200um_coverageminc_label_ops --convert data/imaging/DSURQE_CCFv3_nearest_200um.mnc data/imaging/DSURQE_CCFv3_labels_200um.mnc_bin0.8.mnc \
-#         --outfile data/mouse/cluster_signatures/input_space/mouse_cluster_signatures_rel_mean_threshold${i}_inputspace.csv \
-#         --parallel true \
-#         --nproc 4
-        
-#     echo "Creating human cluster signatures using threshold $i masks and input gene space..."
-
-#     Rscript human_cluster_signatures.R \
-#         --clusterdir data/human/clustering/cluster_masks/absolute/resolution_3.0/mean/threshold_$i/ \
-#         --exprdir data/human/expression/input_space/ \
-#         --metadata data/human/SampleInformation_pipeline_v1.csv \
-#         --template data/human/registration/reference_files/model_3.0mm.mnc \
-#         --outfile data/human/cluster_signatures/input_space/human_cluster_signatures_abs_mean_threshold${i}_inputspace.csv \
-#         --parallel true \
-#         --nproc 4
-        
-#     Rscript human_cluster_signatures.R \
-#         --clusterdir data/human/clustering/cluster_masks/relative/resolution_3.0/mean/threshold_$i/ \
-#         --exprdir data/human/expression/input_space/ \
-#         --metadata data/human/SampleInformation_pipeline_v1.csv \
-#         --template data/human/registration/reference_files/model_3.0mm.mnc \
-#         --outfile data/human/cluster_signatures/input_space/human_cluster_signatures_rel_mean_threshold${i}_inputspace.csv \
-#         --parallel true \
-#         --nproc 4
-# done
+done
 
 
-# Create images containing AHBA microarray samples
-
-# Rscript microarray_sample_image.R \
-# 	--metadata data/human/SampleInformation_pipeline_v1.csv \
-# 	--template data/human/registration/average_to_MNI/mni_icbm152_t1_tal_nlin_sym_09c_extracted.mnc \
-# 	--outdir data/human/expression/ \
-# 	--type labels
-
-# Rscript microarray_sample_image.R \
-# 	--metadata data/human/SampleInformation_pipeline_v1.csv \
-# 	--template data/human/registration/average_to_MNI/mni_icbm152_t1_tal_nlin_sym_09c_extracted.mnc \
-# 	--outdir data/human/expression/ \
-# 	--type mask
-
-# #Transform microarray sample images to study space
-
-# antsApplyTransforms \
-# 	-d 3 \
-# 	--input data/human/expression/AHBA_microarray_samples_mask.mnc \
-# 	--output data/human/expression/AHBA_microarray_samples_mask_studyspace_1.0mm_tmp.mnc \
-# 	--reference-image data/human/registration/average_to_MNI/template_sharpen_shapeupdate_1.0mm.mnc \
-# 	-t [data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc0_GenericAffine.xfm,1] \
-# 	-t data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc1_inverse_NL.xfm
-
-# antsApplyTransforms \
-# 	-d 3 \
-# 	--input data/human/expression/AHBA_microarray_samples_labels.mnc \
-# 	--output data/human/expression/AHBA_microarray_samples_labels_studyspace_1.0mm_tmp.mnc \
-# 	--reference-image data/human/registration/average_to_MNI/template_sharpen_shapeupdate_1.0mm.mnc \
-# 	-t [data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc0_GenericAffine.xfm,1] \
-# 	-t data/human/registration/average_to_MNI/average_to_icbm_nlin_sym_09c_minc1_inverse_NL.xfm
-
-# deactivate
-
-# module load minc-stuffs
-
-# minc_label_ops --convert \
-#     data/human/expression/AHBA_microarray_samples_mask_studyspace_1.0mm_tmp.mnc \
-#     data/human/expression/AHBA_microarray_samples_mask_studyspace_1.0mm.mnc
-# rm data/human/expression/AHBA_microarray_samples_mask_studyspace_1.0mm_tmp.mnc
-
-# minc_label_ops --convert \
-#     data/human/expression/AHBA_microarray_samples_labels_studyspace_1.0mm_tmp.mnc \
-#     data/human/expression/AHBA_microarray_samples_labels_studyspace_1.0mm.mnc
-# rm data/human/expression/AHBA_microarray_samples_labels_studyspace_1.0mm_tmp.mnc
-
-# module purge
+# Generate mouse and human cluster signatures using latent spaces ------------
 
 # for i in ${thresholds[@]};
 # do
@@ -213,6 +192,9 @@ Rscript human_cluster_signatures_dev.R \
 #         --nproc 8
         
 # done
+
+
+
 
 # for i in ${thresholds[@]};
 # do
