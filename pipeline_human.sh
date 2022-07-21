384c5de1c53c48b2764dea8d75a7b30463b4fea3
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-21 14:14:16 -0400

    Added option to run only POND subset

--- pipeline_human.sh
+++ pipeline_human.sh
@@ -17,6 +17,22 @@
 #Activate virtual environment 
 source activate_venv.sh
 
+#Option to use POND subset
+dataset='ALL'
+# dataset=POND
+
+if [ -z $dataset ];
+then
+	human_dir=data/human/
+else
+	if [ $dataset = 'POND' ];
+	then
+		human_dir=data/human/POND/
+	else
+		human_dir=data/human/
+	fi
+fi
+
 
 # Extract compressed jacobian images -----------------------------------------
 
@@ -40,10 +56,11 @@ echo "Calculating human effect sizes using absolute jacobians..."
 Rscript calculate_human_effect_sizes.R \
 	--demographics data/human/registration/DBM_input_demo_passedqc.csv \
 	--imgdir data/human/registration/jacobians/absolute/smooth/ \
-	--outdir data/human/effect_sizes/absolute/resolution_0.5/ \
+	--outdir ${human_dir}/effect_sizes/absolute/resolution_0.5/ \
 	--maskfile data/human/registration/reference_files/mask.mnc \
 	--ncontrols 10 \
-    --threshold 5 \
+	--threshold 5 \
+	--dataset $dataset \
 	--parallel true \
 	--nproc 4
 
@@ -52,10 +69,11 @@ echo "Calculating human effect sizes using relative jacobians..."
 Rscript calculate_human_effect_sizes.R \
 	--demographics data/human/registration/DBM_input_demo_passedqc.csv \
 	--imgdir data/human/registration/jacobians/relative/smooth/ \
-	--outdir data/human/effect_sizes/relative/resolution_0.5/ \
+	--outdir ${human_dir}/effect_sizes/relative/resolution_0.5/ \
 	--maskfile data/human/registration/reference_files/mask.mnc \
 	--ncontrols 10 \
-    --threshold 5 \
+	--threshold 5 \
+	--dataset $dataset \
 	--parallel true \
 	--nproc 4
     
@@ -88,16 +106,16 @@ echo "Resampling absolute effect size images..."
 
 #Downsample absolute effect size images to 3.0mm
 python3 resample_minc_images.py \
-	--imgdir data/human/effect_sizes/absolute/resolution_0.5/ \
-	--outdir data/human/effect_sizes/absolute/resolution_3.0/ \
+	--imgdir ${human_dir}/effect_sizes/absolute/resolution_0.5/ \
+	--outdir ${human_dir}/effect_sizes/absolute/resolution_3.0/ \
 	--isostep 3.0 \
 	--parallel true \
 	--nproc 4
 
 #Downsample absolute effect size images to 1.0mm
 python3 resample_minc_images.py \
-	--imgdir data/human/effect_sizes/absolute/resolution_0.5/ \
-	--outdir data/human/effect_sizes/absolute/resolution_1.0/ \
+	--imgdir ${human_dir}/effect_sizes/absolute/resolution_0.5/ \
+	--outdir ${human_dir}/effect_sizes/absolute/resolution_1.0/ \
 	--isostep 1.0 \
 	--parallel true \
 	--nproc 4
@@ -106,16 +124,16 @@ echo "Resampling relative effect size images..."
 
 #Downsample relative effect size images to 3.0mm
 python3 resample_minc_images.py \
-	--imgdir data/human/effect_sizes/relative/resolution_0.5/ \
-	--outdir data/human/effect_sizes/relative/resolution_3.0/ \
+	--imgdir ${human_dir}/effect_sizes/relative/resolution_0.5/ \
+	--outdir ${human_dir}/effect_sizes/relative/resolution_3.0/ \
 	--isostep 3.0 \
 	--parallel true \
 	--nproc 4
 
 #Downsample relative effect size images to 1.0mm
 python3 resample_minc_images.py \
-	--imgdir data/human/effect_sizes/relative/resolution_0.5/ \
-	--outdir data/human/effect_sizes/relative/resolution_1.0/ \
+	--imgdir ${human_dir}/effect_sizes/relative/resolution_0.5/ \
+	--outdir ${human_dir}/effect_sizes/relative/resolution_1.0/ \
 	--isostep 1.0 \
 	--parallel true \
 	--nproc 4
@@ -127,28 +145,28 @@ echo "Build absolute effect size matrix..."
 
 #Create matrix using absolute effect sizes at 3.0mm
 python3 build_effect_size_matrix.py \
-	--imgdir data/human/effect_sizes/absolute/resolution_3.0/ \
-	--outfile data/human/effect_sizes/absolute/human_effect_sizes_absolute_3.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/absolute/resolution_3.0/ \
+	--outfile ${human_dir}/effect_sizes/absolute/human_effect_sizes_absolute_3.0mm.csv \
 	--mask data/human/registration/reference_files/mask_3.0mm.mnc
     
 #Create matrix using absolute effect sizes at 1.0mm
 python3 build_effect_size_matrix.py \
-	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
-	--outfile data/human/effect_sizes/absolute/human_effect_sizes_absolute_1.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/absolute/resolution_1.0/ \
+	--outfile ${human_dir}/effect_sizes/absolute/human_effect_sizes_absolute_1.0mm.csv \
 	--mask data/human/registration/reference_files/mask_1.0mm.mnc 
     
 echo "Build relative effect size matrix..."    
 
 #Create matrix using absolute effect sizes at 3.0mm
 python3 build_effect_size_matrix.py \
-	--imgdir data/human/effect_sizes/relative/resolution_3.0/ \
-	--outfile data/human/effect_sizes/relative/human_effect_sizes_relative_3.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/relative/resolution_3.0/ \
+	--outfile ${human_dir}/effect_sizes/relative/human_effect_sizes_relative_3.0mm.csv \
 	--mask data/human/registration/reference_files/mask_3.0mm.mnc
 
 #Create matrix using absolute effect sizes at 1.0mm
 python3 build_effect_size_matrix.py \
-	--imgdir data/human/effect_sizes/relative/resolution_1.0/ \
-	--outfile data/human/effect_sizes/relative/human_effect_sizes_relative_1.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/relative/resolution_1.0/ \
+	--outfile ${human_dir}/effect_sizes/relative/human_effect_sizes_relative_1.0mm.csv \
 	--mask data/human/registration/reference_files/mask_1.0mm.mnc
     
     
@@ -158,15 +176,15 @@ echo "Clustering human effect size images..."
     
 #Cluster human effect sizes
 Rscript cluster_human_data.R \
-	--file1 data/human/effect_sizes/absolute/human_effect_sizes_absolute_3.0mm.csv \
-	--file2 data/human/effect_sizes/relative/human_effect_sizes_relative_3.0mm.csv \
+	--file1 ${human_dir}/effect_sizes/absolute/human_effect_sizes_absolute_3.0mm.csv \
+	--file2 ${human_dir}/effect_sizes/relative/human_effect_sizes_relative_3.0mm.csv \
 	--rownames file \
 	--nclusters 10 \
 	--K 10 \
 	--t 20 \
 	--sigma 0.5 \
 	--metric cor \
-	--outfile data/human/clustering/human_clusters_groups10_3.0mm.csv
+	--outfile ${human_dir}/clustering/human_clusters_groups10_3.0mm.csv
 
 
 # Generate representative cluster maps ---------------------------------------
@@ -175,17 +193,17 @@ echo "Creating cluster maps using absolute effect sizes and mean aggregation..."
 
 # Create cluster maps using absolute effect sizes at 3.0mm
 Rscript create_cluster_maps.R \
-	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
-	--imgdir data/human/effect_sizes/absolute/resolution_3.0/ \
-	--outdir data/human/clustering/cluster_maps/absolute/resolution_3.0/mean/ \
+	--clusterfile ${human_dir}/clustering/human_clusters_groups10_3.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/absolute/resolution_3.0/ \
+	--outdir ${human_dir}/clustering/cluster_maps/absolute/resolution_3.0/mean/ \
 	--method mean \
 	--jacobians absolute
     
 # Create cluster maps using absolute effect sizes at 1.0mm
 Rscript create_cluster_maps.R \
-	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
-	--imgdir data/human/effect_sizes/absolute/resolution_1.0/ \
-	--outdir data/human/clustering/cluster_maps/absolute/resolution_1.0/mean/ \
+	--clusterfile ${human_dir}/clustering/human_clusters_groups10_3.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/absolute/resolution_1.0/ \
+	--outdir ${human_dir}/clustering/cluster_maps/absolute/resolution_1.0/mean/ \
 	--method mean \
 	--jacobians absolute
 
@@ -193,17 +211,17 @@ echo "Creating cluster maps using relative effect sizes and mean aggregation..."
 
 # Create cluster maps using relative effect sizes at 3.0mm
 Rscript create_cluster_maps.R \
-	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
-	--imgdir data/human/effect_sizes/relative/resolution_3.0/ \
-	--outdir data/human/clustering/cluster_maps/relative/resolution_3.0/mean/ \
+	--clusterfile ${human_dir}/clustering/human_clusters_groups10_3.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/relative/resolution_3.0/ \
+	--outdir ${human_dir}/clustering/cluster_maps/relative/resolution_3.0/mean/ \
 	--method mean \
 	--jacobians relative
     
 # Create cluster maps using relative effect sizes at 1.0mm
 Rscript create_cluster_maps.R \
-	--clusterfile data/human/clustering/human_clusters_groups10_3.0mm.csv \
-	--imgdir data/human/effect_sizes/relative/resolution_1.0/ \
-	--outdir data/human/clustering/cluster_maps/relative/resolution_1.0/mean/ \
+	--clusterfile ${human_dir}/clustering/human_clusters_groups10_3.0mm.csv \
+	--imgdir ${human_dir}/effect_sizes/relative/resolution_1.0/ \
+	--outdir ${human_dir}/clustering/cluster_maps/relative/resolution_1.0/mean/ \
 	--method mean \
 	--jacobians relative
     
