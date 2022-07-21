fa653c04c5be762aa720ea34389639be42e1231c
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-21 13:22:37 -0400

    Minor formatting

--- get_transcriptomic_files.sh
+++ get_transcriptomic_files.sh
@@ -8,7 +8,8 @@ fi
 
 mouse_expr_dir=/projects/abeauchamp/Projects/MouseHumanMapping/Paper_TranscriptomicSimilarity/main/AMBA/data/
 
-cp ${mouse_expr_dir}MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed.csv ${outdir}MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed.csv
+cp ${mouse_expr_dir}MouseExpressionMatrix_voxel_coronal_maskcoronal_log2_grouped_imputed.csv \
+${outdir}MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed.csv
 
 cp ${mouse_expr_dir}MouseExpressionTree_DSURQE.RData ${outdir}
 
