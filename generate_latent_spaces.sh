2b0778e56caae8696edf9aad88ffca07d8abf1f0
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-25 11:45:20 -0400

    Updated parameters and some reorganization

--- generate_latent_spaces.sh
+++ generate_latent_spaces.sh
@@ -1,59 +1,76 @@
 #!/bin/bash -l
 
-source activate_venv
+source activate_venv.sh
 
+#Input files
+training=data/mouse/expression/MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_GM_labelled_67_scaled.csv
+mouse_input=data/mouse/expression/input_space/MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_scaled.csv
+human_input=data/human/expression/input_space/HumanExpressionMatrix_samples_pipeline_abagen_homologs_scaled.csv
+
+#Output directories
 outdir=data/MLP_outcomes/
-
 if [ ! -d "$outdir" ]; then
-	mkdir "$outdir"
+	mkdir -p "$outdir"
 fi
 
+mouse_latent_space=data/mouse/expression/latent_space/
+if [ ! -d "$mouse_latent_space" ]; then
+	mkdir -p "$mouse_latent_space"
+fi
+
+human_latent_space=data/human/expression/latent_space/
+if [ ! -d "$human_latent_space" ]; then
+	mkdir -p "$human_latent_space"
+fi
+
+#Parameters file
 current_date=$(date +'%Y%m%d')
 paramsfile=${outdir}MLP_params_${current_date}.csv
 touch $paramsfile
 
+nunits=200
+L2=0.0
+
 for i in {1..500}
 do
- echo "Iteration $i"
+	echo "Iteration $i"
 
- if [ $i -eq 1 ];
- then
-	 paramsheader=true
- else
-	 paramsheader=false
- fi
+	if [ $i -eq 1 ];
+	then
+		paramsheader=true
+	else
+		paramsheader=false
+	fi
 
- python3 train_multilayer_perceptron.py \
-	 --outdir $outdir \
-	 --training data/MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_GM_labelled_67_scaled.csv \
-	 --mousetransform data/MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_scaled.csv \
-	 --humantransform data/HumanExpressionMatrix_samples_pipeline_v1_homologs_scaled.csv \
-	 --nunits 200 \
-	 --L2 1e-6 \
-	 --nepochs 200 \
-	 --learningrate 1e-5 \
-	 --confusionmatrix true \
-	 --seed $i \
-	 --saveparams true \
-	 --paramsheader $paramsheader \
-	 --verbose true
+	python3 train_multilayer_perceptron.py \
+		--outdir $outdir \
+		--training $training \
+		--mousetransform $mouse_input \
+		--humantransform $human_input \
+		--nunits $nunits \
+		--L2 $L2 \
+		--nepochs 25 \
+		--totalsteps 50 \
+		--learningrate 0.01 \
+		--confusionmatrix false \
+		--seed $i \
+		--saveparams true \
+		--paramsheader $paramsheader \
+		--verbose true
 
- cat ${outdir}MLP_params.csv >> $paramsfile
+	cat ${outdir}MLP_params.csv >> $paramsfile
 
- mv ${outdir}MLP_labels67_layers3_units200_L21e-06_confusionmatrix_training.csv \
-	 ${outdir}MLP_labels67_layers3_units200_L21e-06_confusionmatrix_training_$i.csv
+	mv ${outdir}MLP_labels67_layers3_units${nunits}_L2${L2}_humanprob.csv \
+		${outdir}MLP_labels67_layers3_units${nunits}_L2${L2}_humanprob_$i.csv
 
- mv ${outdir}MLP_labels67_layers3_units200_L21e-06_humanprob.csv \
-	 ${outdir}MLP_labels67_layers3_units200_L21e-06_humanprob_$i.csv
+	mv ${outdir}MLP_labels67_layers3_units${nunits}_L2${L2}_humantransform.csv \
+		${human_latent_space}MLP_labels67_layers3_units${nunits}_L2${L2}_humantransform_$i.csv
 
- mv ${outdir}MLP_labels67_layers3_units200_L21e-06_humantransform.csv \
-	 ${outdir}MLP_labels67_layers3_units200_L21e-06_humantransform_$i.csv
+	mv ${outdir}MLP_labels67_layers3_units${nunits}_L2${L2}_mouseprob.csv \
+		${outdir}MLP_labels67_layers3_units${nunits}_L2${L2}_mouseprob_$i.csv
 
- mv ${outdir}MLP_labels67_layers3_units200_L21e-06_mouseprob.csv \
-	 ${outdir}MLP_labels67_layers3_units200_L21e-06_mouseprob_$i.csv
-
- mv ${outdir}MLP_labels67_layers3_units200_L21e-06_mousetransform.csv \
-	 ${outdir}MLP_labels67_layers3_units200_L21e-06_mousetransform_$i.csv
+	mv ${outdir}MLP_labels67_layers3_units${nunits}_L2${L2}_mousetransform.csv \
+		${mouse_latent_space}MLP_labels67_layers3_units${nunits}_L2${L2}_mousetransform_$i.csv
 
 done
 
