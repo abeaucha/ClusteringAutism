c85b832690021d7268df4d091d892dceb19017a9
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-07-26 09:36:53 -0400

    Removed all commands except those that create symlinks for 100 latent spaces

--- organize_expression_files.sh
+++ organize_expression_files.sh
@@ -1,46 +1,5 @@
 #!/bin/bash
 
-#Create mouse expression directory
-if [ ! -d "data/mouse/expression/" ]; then
-    mkdir data/mouse/expression
-fi
-
-#Create mouse input space directory
-if [ ! -d "data/mouse/expression/input_space/" ]; then
-    mkdir data/mouse/expression/input_space/
-fi
-
-#Create symlink for input space data
-cd data/mouse/expression/input_space/
-input_space=MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_scaled.csv
-if [ ! -f $input_space ];
-then
-    ln -s ../../../$input_space ./
-fi
-
-#Return to main directory
-cd ../../../..
-
-#Create mouse latent space directory
-if [ ! -d "data/mouse/expression/latent_space/" ]; then
-    mkdir data/mouse/expression/latent_space/
-fi
-
-#Create symlinks for mouse latent space data
-cd data/mouse/expression/latent_space/
-mouse_latent_spaces=(../../../MLP_outcomes/*mousetransform*.csv)
-for file in ${mouse_latent_spaces[@]}; 
-do
-    filename=$(basename -- "$file")
-    if [ ! -f $filename ];
-    then
-        ln -s $file ./
-    fi
-done
-
-#Return to main directory
-cd ../../../..
-
 if [ ! -d 'data/mouse/expression/latent_space_100/' ];
 then
     mkdir data/mouse/expression/latent_space_100
@@ -48,49 +7,8 @@ fi
 
 #Create symlinks for mouse latent space data
 cd data/mouse/expression/latent_space_100/
-mouse_latent_spaces=(../../../MLP_outcomes/*mousetransform_1[0-9][0-9].csv)
+mouse_latent_spaces=(../latent_space/*mousetransform_1[0-9][0-9].csv)
 for file in ${mouse_latent_spaces[@]}; 
-do
-    filename=$(basename -- "$file")
-    if [ ! -f $filename ];
-    then
-        ln -s $file ./
-    fi
-done
-
-#Return to main directory
-cd ../../../..
-
-#Create human expression directory
-if [ ! -d "data/human/expression/" ]; then
-    mkdir data/human/expression
-fi
-
-#Create human input space directory
-if [ ! -d "data/human/expression/input_space/" ]; then
-    mkdir data/human/expression/input_space/
-fi
-
-# #Create symlink for input space data
-cd data/human/expression/input_space/
-input_space=HumanExpressionMatrix_samples_pipeline_v1_homologs_scaled.csv
-if [ ! -f $input_space ];
-then
-    ln -s ../../../$input_space ./
-fi
-
-#Return to main directory
-cd ../../../..
-
-#Create human latent space directory
-if [ ! -d "data/human/expression/latent_space/" ]; then
-    mkdir data/human/expression/latent_space/
-fi
-
-#Create symlinks for human latent space data
-cd data/human/expression/latent_space/
-human_latent_spaces=(../../../MLP_outcomes/*humantransform*.csv)
-for file in ${human_latent_spaces[@]}; 
 do
     filename=$(basename -- "$file")
     if [ ! -f $filename ];
@@ -109,7 +27,7 @@ then
 fi
 
 cd data/human/expression/latent_space_100/
-human_latent_spaces=(../../../MLP_outcomes/*humantransform_1[0-9][0-9].csv)
+human_latent_spaces=(../latent_space/*humantransform_1[0-9][0-9].csv)
 for file in ${human_latent_spaces[@]}; 
 do
     filename=$(basename -- "$file")
