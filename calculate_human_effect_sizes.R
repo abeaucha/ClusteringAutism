fa1bc63252a290b92b7d4d7320d318ecb5623b57
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2022-12-02 13:36:15 -0500

    Updated script to process combinations of the data cohorts

--- calculate_human_effect_sizes.R
+++ calculate_human_effect_sizes.R
@@ -26,35 +26,41 @@ suppressPackageStartupMessages(library(tcltk))
 # Command line arguments -----------------------------------------------------
 
 option_list <- list(
-  make_option('--demographics',
-              type = 'character',
+  make_option("--demographics",
+              type = "character",
               help = "Path to CSV file containing human demographics data."),
-  make_option('--imgdir',
-              type = 'character',
+  make_option("--imgdir",
+              type = "character",
               help = paste("Path to directory containing images to use",
                            "to compute effect sizes.")),
-  make_option('--maskfile',
-              type = 'character',
+  make_option("--maskfile",
+              type = "character",
               help = "Path to mask file for the images."),
-  make_option('--outdir',
-              type = 'character',
+  make_option("--outdir",
+              type = "character",
               help = paste("Path to directory in which to save the effect",
                            "size images.")),
-  make_option('--ncontrols',
+  make_option("--ncontrols",
               type = 'numeric',
               default = 10,
               help = paste("Number of controls to use when computing effect",
                            "sizes. [default %default]")),
-  make_option('--threshold',
-              type = 'numeric',
+  make_option("--threshold",
+              type = "numeric",
               default = 10,
               help = paste("[default %default]")),
-  make_option('--parallel',
-              type = 'character',
+  make_option("--dataset",
+              type = "numeric",
+              default = 1,
+              help = paste("A numeric flag indicating which data to use.", 
+                           "Set to 1 for all data. Set to 2 for POND and",
+                           "SickKids. Set to 3 for POND only. [default %default]")),
+  make_option("--parallel",
+              type = "character",
               default = 'false',
               help = "Option to run in parallel. [default %default]"),
-  make_option('--nproc',
-              type = 'numeric',
+  make_option("--nproc",
+              type = "numeric",
               help = paste("Number of processors to use in parallel.",
                            "Ignored if --parallel is false."))
 ) 
@@ -67,24 +73,22 @@ option_list <- list(
 #' @param participant (data.frame) A data.frame row containing demographic 
 #' information for the participant of interest. 
 #' @param controls (data.frame) Demographic information for the controls.
-#' @param imgfiles (character vector) A set of paths to MINC image files that 
-#' include (but are not limited to) all control images.
 #' @param ncontrols (numeric scalar) Number of controls to use for propensity
 #' matching
 #' @param threshold (numeric scalar) 
 #' @param seed (numeric scalar) Random seed to use for control sampling.
 #'
-#' @return (character vector) The paths to the images of the propensity-
-#' matched controls.
-get_control_files <- function(participant, controls, imgfiles,
-                              ncontrols = 10, threshold = 10, seed = NULL){
+#' @return (data.frame) Demographic information for the propensity-matched 
+#' controls.
+get_matched_controls <- function(participant, controls, ncontrols = 10, 
+                                 threshold = 10, seed = NULL){
   
   if (!is.null(seed)){set.seed(seed)}
   
-  participant_age <- pull(participant, 'Age')
-  participant_sex <- pull(participant, 'Sex')
-  participant_site <- pull(participant, 'Site')
-  participant_scanner <- pull(participant, 'Scanner')
+  participant_age <- pull(participant, "Age")
+  participant_sex <- pull(participant, "Sex")
+  participant_site <- pull(participant, "Site")
+  participant_scanner <- pull(participant, "Scanner")
   
   controls_tmp <- controls %>% 
     filter(Sex == participant_sex,
@@ -109,50 +113,36 @@ get_control_files <- function(participant, controls, imgfiles,
     }
   }
   
-  controls <- controls_tmp
-  
-  control_ids <- controls %>%
+  controls <- controls_tmp %>%
     mutate(AgeDiff = abs(Age - participant_age)) %>% 
     top_n(n = -1*ncontrols, wt = AgeDiff) %>% 
-    sample_n(size = ncontrols, replace = FALSE) %>% 
-    pull(Extract_ID) %>% 
-    str_remove('.mnc')
+    sample_n(size = ncontrols, replace = FALSE) 
   
-  control_files <- character(length(control_ids))
-  for (j in 1:length(control_ids)){
-    control_files[j] <- str_subset(imgfiles, control_ids[j])
-  }
-  
-  return(control_files)
+  return(controls)
   
 }
 
 
-#' Compute the effect size for a participant
+#' Compute the effect size for a participant.
 #'
-#' @param participant (data.frame) A data.frame row containing demographic 
-#' information for the participant of interest. 
-#' @param controls (character vector) Paths to the MINC images of the 
-#' propensity-matched controls.
-#' @param imgfiles (character vector) Paths to image MINC files including 
-#' (but not limited to) the participant of interest.
+#' @param participant (character scalar) Path to the MINC volume of 
+#' interest.
+#' @param controls (character vector) Paths to the MINC volumes of the 
+#' controls.
 #' @param mask (character scalar) Path to mask image MINC file. 
 #'
 #' @return (mincSingleDim) A vector of voxel-wise effect size values.
-compute_effect_size <- function(participant, controls, imgfiles, mask) {
+compute_effect_size <- function(participant, controls, mask) {
   
+  sink(file = nullfile(), type = "output")
   control_mean <- mincMean(filenames = controls)
   control_mean <- control_mean[,1]
   
   control_sd <- mincSd(filenames = controls)
   control_sd <- control_sd[,1]
+  sink(file = NULL)
   
-  participant_id <- participant[, 'Extract_ID'][[1]] %>%
-    str_remove('.mnc')
-  
-  participant_file <- str_subset(imgfiles, participant_id)
-  
-  participant_vol <- mincGetVolume(participant_file)
+  participant_vol <- mincGetVolume(participant)
   
   participant_z <- (participant_vol - control_mean)/control_sd
   
@@ -174,42 +164,63 @@ compute_effect_size <- function(participant, controls, imgfiles, mask) {
 #' information for the participant of interest.
 #' @param controls (data.frame) Demographic information for the controls.
 #' @param ncontrols (numeric scalar) Number of controls to use for propensity
-#' matching
+#' matching.
 #' @param threshold (numeric scalar) 
-#' @param mask (character scalar) Path to mask image MINC file. 
-#' @param imgdir (character vector) Paths to image MINC files.
+#' @param mask (character scalar) Path to mask volume. 
+#' @param imgdir (character scalar) Path to the directory containing the 
+#' image files.
 #' @param outdir (character scalar) Path to directory in which to save effect 
-#' size MINC image.
+#' sizes.
 #' @param seed (numeric scalar) Random seed to use for control sampling.
 #'
 #' @return (numeric scalar) Value of 1 if function succeeds.
 executor <- function(participant, controls, ncontrols = 10, 
-                     threshold = 10, mask, imgdir, outdir, seed = NULL) {
+                     threshold = 10, mask, imgdir, outdir,
+                     dataset = NULL, seed = NULL) {
   
-  imgfiles <- list.files(imgdir, full.names = TRUE) %>% 
-    str_subset('.mnc')
+  #Get propensity-matched controls
+  controls_matched <- get_matched_controls(participant = participant,
+                                           controls = controls,
+                                           ncontrols = ncontrols,
+                                           threshold = threshold,
+                                           seed = seed)
   
-  control_files <- get_control_files(participant = participant,
-                                     controls = controls,
-                                     imgfiles = imgfiles,
-                                     ncontrols = ncontrols,
-                                     threshold = threshold,
-                                     seed = seed)
+  #Extract image files
+  imgfiles <- str_subset(list.files(imgdir, full.names = TRUE), ".mnc")
   
-  effect_size <- compute_effect_size(participant = participant,
+  #Image files for the controls
+  control_ids = str_remove(controls_matched[["Extract_ID"]], ".mnc")
+  control_files <- character(length(control_ids))
+  for (j in 1:length(control_ids)){
+    control_files[j] <- str_subset(imgfiles, control_ids[j])
+  }
+  
+  #Image file for the participant
+  participant_id <- str_remove(participant[[1, "Extract_ID"]], ".mnc")
+  participant_file <- str_subset(imgfiles, participant_id)
+  
+  #Compute the effect size volume
+  effect_size <- compute_effect_size(participant = participant_file,
                                      controls = control_files,
-                                     imgfiles = imgfiles,
                                      mask = mask)
   
-  outfile <- attributes(effect_size)[['filename']] %>%
-    basename() %>%
-    str_replace('.mnc', str_c('_effectsize_ncontrols', ncontrols, '.mnc'))
-  outfile <- file.path(outdir, outfile)
+  #Get the resolution
+  res <- max(minc.separation.sizes(participant_file))
   
+  #Output file
+  outfile <- participant_id %>% 
+    basename() %>% 
+    str_c("ES", 
+          "res", res,
+          "data", dataset, 
+          "nc", ncontrols, 
+          "thresh", threshold, sep = "_") %>% 
+    str_c(".mnc")
+  outfile <- file.path(outdir, outfile)
   mincWriteVolume(effect_size,
                   output.filename = outfile,
                   clobber = TRUE)
-  
+
   return(1)
 } 
 
@@ -218,13 +229,14 @@ executor <- function(participant, controls, ncontrols = 10,
 
 #Parse command line args
 args <- parse_args(OptionParser(option_list = option_list))
-demofile <- args[['demographics']]
-imgdir <- args[['imgdir']]
-maskfile <- args[['maskfile']]
-outdir <- args[['outdir']]
-ncontrols <- args[['ncontrols']]
-threshold <- args[['threshold']]
-inparallel <- ifelse(args[['parallel']] == 'true', TRUE, FALSE)
+demographics <- args[["demographics"]]
+imgdir <- args[["imgdir"]]
+maskfile <- args[["maskfile"]]
+outdir <- args[["outdir"]]
+ncontrols <- args[["ncontrols"]]
+threshold <- args[["threshold"]]
+dataset <- args[["dataset"]]
+inparallel <- ifelse(args[["parallel"]] == "true", TRUE, FALSE)
 
 #Create outdir if needed
 if (!dir.exists(outdir)) {
@@ -232,12 +244,24 @@ if (!dir.exists(outdir)) {
 }
 
 #Import demographics data
-demographics <- data.table::fread(demofile, header = TRUE) %>% 
-  as_tibble()
+demographics <- as_tibble(data.table::fread(demographics, header = TRUE))
 
-#Filter for POND and SickKids 
-demographics <- demographics %>% 
-  filter(Dataset %in% c("POND", "SickKids"))
+#Filter data if desired
+if (is.numeric(dataset)) {
+  if (dataset != 1) {
+    if (dataset == 2){
+      demographics <- demographics %>% 
+        filter(Dataset %in% c("POND", "SickKids"))
+    } else if (dataset == 3) {
+      demographics <- demographics %>% 
+        filter(Dataset == "POND")
+    } else {
+      stop(str_c("Argument --dataset must be one of {1, 2, 3}: ", dataset))
+    }
+  }
+} else {
+  stop(str_c("Argument --dataset must be one of {1, 2, 3}: ", dataset))
+}
 
 #Remove entries with missing diagnosis, age, or sex
 demographics <- demographics %>% 
@@ -249,22 +273,22 @@ demographics <- demographics %>%
 
 #Extract control demographics 
 controls <- demographics %>% 
-  filter(DX == 'Control')
+  filter(DX == "Control")
 
 #Extract participant demographics
 participants <- demographics %>% 
-  filter(DX != 'Control')
+  filter(DX != "Control")
 
 #Compute effect sizes
 pb <- txtProgressBar(max = nrow(participants), style = 3)
 progress <- function(n) {setTxtProgressBar(pb = pb, value = n)}
 if (inparallel) {
-  nproc <- args[['nproc']]
+  nproc <- args[["nproc"]]
   cl <- makeSOCKcluster(nproc)
   registerDoSNOW(cl)
   opts <- list(progress=progress)
   tmp <- foreach(i = 1:nrow(participants), 
-                 .packages = c('tidyverse', 'RMINC'),
+                 .packages = c("tidyverse", "RMINC"),
                  .options.snow=opts) %dopar% {
                    executor(participant = participants[i,],
                             controls = controls,
@@ -273,13 +297,14 @@ if (inparallel) {
                             mask = maskfile,
                             imgdir = imgdir,
                             outdir = outdir,
+                            dataset = dataset,
                             seed = i)
                  } 
   close(pb)
   stopCluster(cl)
 } else {
   tmp <- foreach(i = 1:nrow(participants), 
-                 .packages = c('tidyverse', 'RMINC')) %do% {
+                 .packages = c("tidyverse", "RMINC")) %do% {
                    progress(n = i)
                    executor(participant = participants[i,],
                             controls = controls,
@@ -288,6 +313,7 @@ if (inparallel) {
                             mask = maskfile,
                             imgdir = imgdir,
                             outdir = outdir,
+                            dataset = dataset,
                             seed = i)
                  }
   close(pb)
