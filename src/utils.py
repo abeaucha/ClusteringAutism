4c9003fdfc592c382795af404198ca7ad4fdb2ac
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-02-22 15:34:07 -0500

    Moved some processing functions to utils module

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
-import subprocess
 import os
-import numpy  as np
-import pandas as pd
-from random   import randint
-from warnings import warn
+import subprocess
+import numpy   as np
+import pandas  as pd
+from functools import partial
+from random    import randint
+from re        import sub
+from tqdm      import tqdm
+from warnings  import warn
 
 
 def execute_R(script, args):
@@ -201,3 +204,207 @@ def random_id(n):
     fstring = '{:0'+str(n)+'}'
     randid = fstring.format(num)
     return randid
+
+
+def gunzip_file(gzfile, keep = True, outdir = None):
+    
+    """
+    Unzip a compressed file.
+    
+    Arguments
+    ---------
+    gzfile: str
+        Path to the file to unzip.
+    keep: bool
+        Option to keep the compressed file after extraction.
+    outdir: str
+        Path to the output directory. If None, the file will
+        be unzipped in its native directory.
+        
+    Returns
+    -------
+    outfile: str
+        Path to the unzipped file.
+    """
+    
+    os.system('gunzip -f -k {}'.format(gzfile))
+    outfile = sub(r'.gz', '', gzfile)
+    if not keep:
+        os.system('rm {}'.format(gzfile))
+    if outdir is not None:
+        outdir = os.path.join(outdir, '')
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        os.system('mv {} {}'.format(outfile, outdir))
+        outfile = os.path.join(outdir, os.path.basename(outfile))
+
+    return outfile
+
+
+def gunzip_files(infiles, keep = True, outdir = None, 
+                 parallel = False, nproc = None):
+    
+    """
+    Unzip a set of compressed files.
+    
+    Arguments
+    ---------
+    infiles: list
+        List of paths to files to unzip.
+    keep: bool
+        Option to keep the compressed files after extraction.
+    outdir: None
+        Path to the output directory. If None, the files will be 
+        unzipped in their native directories.
+    parallel: bool
+        Option to run in parallel.
+    nprpc: int
+        Number of processors to use in parallel.
+        
+    Returns
+    -------
+    outfiles: list
+        List of paths to the unzipped files.
+    """
+    
+    gunzip_file_partial = partial(gunzip_file, 
+                                  keep = keep,
+                                  outdir = outdir)
+    if parallel:
+        if nproc is None:
+            raise ValueError("Set the nproc argument to specify the number ",
+                             "of processors to use")
+        pool = mp.Pool(nproc)
+        outfiles = []
+        for outfile in tqdm(pool.imap(gunzip_file_partial, infiles), total = len(infiles)):
+            outfiles.append(outfile)
+        pool.close()
+        pool.join()
+    else:
+        outfiles = list(map(gunzip_file_partial, tqdm(infiles)))
+        
+    return outfiles
+
+
+def nii_to_mnc(infile, keep = True, outdir = None):
+    
+    """
+    Convert a NIFTY file to MINC format.
+    
+    Arguments
+    ---------
+    infile: str
+        Path to the NIFTY file to convert.
+    outdir: str
+        Path to the output directory. If None, the output MINC file 
+        will be stored in the native directory.
+    keep: bool
+        Option to keep the input file. 
+        
+    Returns
+    -------
+    outfile: str
+        Path to the converted MINC file.
+    """
+    
+    os.system('nii2mnc -quiet -clobber {}'.format(infile))
+    outfile = sub(r'.nii', '.mnc', infile)
+    if not keep:
+        os.system('rm {}'.format(infile))
+    if outdir is not None:
+        outdir = os.path.join(outdir, '')
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        os.system('mv {} {}'.format(outfile, outdir))
+        outfile = os.path.join(outdir, os.path.basename(outfile))
+        
+    return outfile
+
+
+def mnc_to_nii(infile, keep = True, outdir = None):
+    
+    """
+    Convert a MINC file to NIFTY format.
+    
+    Arguments
+    ---------
+    infile: str
+        Path to the MINC file to convert.
+    outdir: str
+        Path to the output directory. If None, the output MINC file 
+        will be stored in the native directory.
+    keep: bool
+        Option to keep the input file. 
+        
+    Returns
+    -------
+    outfile: str
+        Path to the converted NIFTY file.
+    """
+    
+    os.system('mnc2nii -quiet {}'.format(infile))
+    outfile = sub(r'.mnc', '.nii', infile)
+    if not keep:
+        os.system('rm {}'.format(infile))
+    if outdir is not None:
+        outdir = os.path.join(outdir, '')
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        os.system('mv {} {}'.format(outfile, outdir))
+        outfile = os.path.join(outdir, os.path.basename(outfile))
+        
+    return outfile
+    
+    
+def convert_images(infiles, input_format = 'nifty', output_format = 'minc', 
+                   keep = True, outdir = None, parallel = False, nproc = None):
+    
+    """
+    Convert a set of images between NIFTY and MINC formats.
+    
+    Arguments
+    ---------
+    infiles: list
+        List of paths to images to convert.
+    input_format: str
+        One of {'nifty', 'minc'} indicating the input format.
+    output_format: str
+        One of {'nifty', 'minc'} indicating the output format. 
+        Must be different from the input format.
+    keep: bool
+        Option to keep the input file. 
+    outdir: str
+        Path to the output directory. If None, the converted
+        images will be stored in the native directory.
+    parallel: bool
+        Option to run in parallel.
+    nprpc: int
+        Number of processors to use in parallel.
+        
+    Returns
+    -------
+    outfiles: list
+        List of paths to the converted images.
+    """
+        
+    if (input_format == 'nifty') & (output_format == 'minc'):
+        converter = partial(nii_to_mnc, keep = keep, outdir = outdir)
+    elif (input_format == 'minc') & (output_format == 'nifty'):
+        converter = partial(mnc_to_nii, keep = keep, outdir = outdir)
+    else:
+        raise ValueError 
+        
+    if parallel:
+        if nproc is None:
+            raise ValueError("Set the nproc argument to specify the number ",
+                             "of processors to use")
+        pool = mp.Pool(nproc)
+        outfiles = []
+        for outfile in tqdm(pool.imap(converter, infiles), total = len(infiles)):
+            outfiles.append(outfile)
+        pool.close()
+        pool.join()
+    else:
+        outfiles = list(map(converter, tqdm(infiles)))
+        
+    return outfiles
