134d1e0d7d50870765ca6093e57958f5f1510680
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-03-14 13:47:25 -0400

    Module to compute pairwise similarity between images

--- src/transcriptomic.py
+++ src/transcriptomic.py
@@ -4,12 +4,13 @@ import sys
 import multiprocessing as mp
 import numpy as np
 import pandas as pd
+import processing
 from datatable import fread
-from functools import partial
-from io                     import StringIO
+from io import StringIO
 from pyminc.volumes.factory import volumeFromFile
-from src import processing
 from tqdm import tqdm
+from itertools import product
+from functools import partial
 
 
 @contextlib.contextmanager
@@ -22,25 +23,24 @@ def silence():
 
 def mouse_signature(img, expr, mask, signed = True, threshold = 'top_n',
                     threshold_value = 0.2, threshold_symmetric = True):
-
     """
     Compute the transcriptomic signature for a mouse image
     """
 
-    #Import image
+    # Import image
     img = processing.import_image(img = img, mask = mask, flatten = False)
 
-    #Import brain mask
+    # Import brain mask
     mask = processing.import_image(img = mask, flatten = True)
 
-    #Import transcriptomic data
+    # Import transcriptomic data
     with silence():
         expr = fread(expr, header = True).to_pandas()
     if np.sum(mask) != expr.shape[0]:
         raise Exception("The number of voxels in mask does not match the "
                         "number of rows in expr.")
 
-    #Threshold the image if desired
+    # Threshold the image if desired
     if threshold is not None:
         img = processing.threshold_image(img = img,
                                          method = threshold,
@@ -52,12 +52,12 @@ def mouse_signature(img, expr, mask, signed = True, threshold = 'top_n',
                                          threshold = 0,
                                          symmetric = True)
 
-    #Create a mask from the (thresholded) image
+    # Create a mask from the (thresholded) image
     img_mask = processing.mask_from_image(img = img, signed = signed)
     img_mask = img_mask.flatten()
     img_mask = img_mask[mask == 1]
 
-    #If signed, return signatures for positive and negative image values
+    # If signed, return signatures for positive and negative image values
     if signed:
         signature_pos = expr.loc[img_mask == 1].mean().to_numpy()
         signature_neg = expr.loc[img_mask == -1].mean().to_numpy()
@@ -70,22 +70,21 @@ def mouse_signature(img, expr, mask, signed = True, threshold = 'top_n',
 
 def human_signature(img, expr, mask, coords, signed = True, threshold = 'top_n',
                     threshold_value = 0.2, threshold_symmetric = True):
-
     """
     Compute the transcriptomic signature for a human image
     """
 
-    #Import image
+    # Import image
     img = processing.import_image(img = img, mask = mask, flatten = False)
 
-    #Import transcriptomic data
+    # Import transcriptomic data
     with silence():
         expr = fread(expr, header = True).to_pandas()
 
-    #Import microarray coordinates
+    # Import microarray coordinates
     coords = pd.read_csv(coords)
 
-    #Threshold the image if desired
+    # Threshold the image if desired
     if threshold is not None:
         img = processing.threshold_image(img = img,
                                          method = threshold,
@@ -97,20 +96,20 @@ def human_signature(img, expr, mask, coords, signed = True, threshold = 'top_n',
                                          threshold = 0,
                                          symmetric = True)
 
-    #Create a mask from the (thresholded) image
+    # Create a mask from the (thresholded) image
     img_mask = processing.mask_from_image(img = img, signed = signed)
 
-    #Create a mask for voxels with microarray date
+    # Create a mask for voxels with microarray date
     vol = volumeFromFile(mask)
     microarray_mask = np.zeros(coords.shape[0], dtype = int)
     for i, row in coords.iterrows():
         coords_world = np.array([row['x'], row['y'], row['z']])
         coords_voxel = vol.convertWorldToVoxel(coords_world)
         coords_voxel = np.round(coords_voxel)
-        x = int(coords_voxel[0])-1
-        y = int(coords_voxel[1])-1
-        z = int(coords_voxel[2])-1
-        microarray_mask[i] = img_mask[x,y,z]
+        x = int(coords_voxel[0]) - 1
+        y = int(coords_voxel[1]) - 1
+        z = int(coords_voxel[2]) - 1
+        microarray_mask[i] = img_mask[x, y, z]
     vol.closeVolume()
 
     if signed:
@@ -124,7 +123,6 @@ def human_signature(img, expr, mask, coords, signed = True, threshold = 'top_n',
 
 
 def similarity(x, y, metric = 'correlation'):
-
     """
     Compute the similarity between two vectors
     """
@@ -140,13 +138,13 @@ def similarity(x, y, metric = 'correlation'):
 
     if metric == 'correlation':
         d = np.corrcoef(x = x, y = y)
-        d = d[0,1]
+        d = d[0, 1]
     elif metric == 'cosine':
-        d = np.dot(x, y)/np.sqrt((np.dot(x, x)*np.dot(y,y)))
+        d = np.dot(x, y) / np.sqrt((np.dot(x, x) * np.dot(y, y)))
     elif metric == 'euclidean':
-        d = np.linalg.norm(x-y, ord = 2)
+        d = np.linalg.norm(x - y, ord = 2)
     elif metric == 'manhattan':
-        d = np.sqrt(np.linalg.norm(x-y, ord = 1))
+        d = np.sqrt(np.linalg.norm(x - y, ord = 1))
     elif metric == 'KL-divergence':
         d = np.sum(np.where(x != 0, x * np.log(x / y), 0))
     else:
@@ -157,9 +155,9 @@ def similarity(x, y, metric = 'correlation'):
 
 def compute_transcriptomic_similarity(expr, imgs, masks, microarray_coords,
                                       metric = 'correlation', signed = True,
-                                      threshold = 'top_n', threshold_value = 0.2,
+                                      threshold = 'top_n',
+                                      threshold_value = 0.2,
                                       threshold_symmetric = True):
-
     """
     Compute the similarity between a mouse image and a human image. 
     
@@ -203,7 +201,6 @@ def compute_transcriptomic_similarity(expr, imgs, masks, microarray_coords,
 
 
 def get_latent_spaces(expr, ids = None):
-
     latent_spaces = []
     for e in expr:
         e = os.path.join(e, 'latent_space')
@@ -212,7 +209,8 @@ def get_latent_spaces(expr, ids = None):
                    [os.path.splitext(file)[0] for file in files]]
         ids_files_all = sorted(zip(ids_all, files))
         if ids is not None:
-            ls = [os.path.join(e, file) for i, file in ids_files_all if i in ids]
+            ls = [os.path.join(e, file) for i, file in ids_files_all if
+                  i in ids]
         else:
             ls = [os.path.join(e, file) for i, file in ids_files_all]
         latent_spaces.append(ls)
@@ -227,12 +225,12 @@ def transcriptomic_similarity(imgs, expr, masks, microarray_coords,
                               metric = 'correlation', signed = True,
                               threshold = 'top_n', threshold_value = 0.2,
                               threshold_symmetric = True, show_progress = True):
-
     if gene_space == 'homologous-genes':
 
         expr = [i for i in expr]
-        expr_files = ('MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_scaled.csv',
-                      'HumanExpressionMatrix_samples_pipeline_abagen_homologs_scaled.csv')
+        expr_files = (
+            'MouseExpressionMatrix_voxel_coronal_log2_grouped_imputed_homologs_scaled.csv',
+            'HumanExpressionMatrix_samples_pipeline_abagen_homologs_scaled.csv')
         for i, path in enumerate(expr):
             expr[i] = os.path.join(path, 'input_space', expr_files[i])
         expr = (expr[0], expr[1])
@@ -264,20 +262,20 @@ def transcriptomic_similarity(imgs, expr, masks, microarray_coords,
     elif gene_space == 'average-latent-space':
 
         expr = get_latent_spaces(expr = expr,
-                                 ids = range(1, n_latent_spaces+1))
+                                 ids = range(1, n_latent_spaces + 1))
         if show_progress: expr = tqdm(expr)
 
         sim = []
         for e in expr:
             s = compute_transcriptomic_similarity(imgs = imgs,
-                                                    expr = e,
-                                                    masks = masks,
-                                                    microarray_coords = microarray_coords,
-                                                    signed = signed,
-                                                    metric = metric,
-                                                    threshold = threshold,
-                                                    threshold_value = threshold_value,
-                                                    threshold_symmetric = threshold_symmetric)
+                                                  expr = e,
+                                                  masks = masks,
+                                                  microarray_coords = microarray_coords,
+                                                  signed = signed,
+                                                  metric = metric,
+                                                  threshold = threshold,
+                                                  threshold_value = threshold_value,
+                                                  threshold_symmetric = threshold_symmetric)
             sim.append(s)
         sim = np.mean(sim)
 
@@ -286,3 +284,43 @@ def transcriptomic_similarity(imgs, expr, masks, microarray_coords,
                          "{'homologous-genes', 'latent-space', 'average-latent-space'}")
 
     return sim
+
+
+def pairwise_transcriptomic_similarity(imgs, expr, masks, microarray_coords,
+                                       gene_space = 'average-latent-space',
+                                       n_latent_spaces = 100, latent_space_id = 1,
+                                       metric = 'correlation', signed = True,
+                                       threshold = 'top_n', threshold_value = 0.2,
+                                       threshold_symmetric = True, parallel = False,
+                                       nproc = None):
+
+    partial_args = locals().copy()
+    partial_args['show_progress'] = False
+    del partial_args['imgs']
+    del partial_args['parallel']
+    del partial_args['nproc']
+
+    evaluator = partial(transcriptomic_similarity,
+                        **partial_args)
+
+    pairs = list(product(imgs[0], imgs[1]))
+    pairs = pairs[:5]
+
+    if parallel:
+        if nproc is None:
+            raise ValueError("Set the nproc argument to specify the "
+                             "number of processors to use in parallel.")
+        pool = mp.Pool(nproc)
+        sim = []
+        for s in tqdm(pool.imap(evaluator, pairs), total = len(pairs)):
+            sim.append(s)
+        pool.close()
+        pool.join()
+    else:
+        sim = list(map(evaluator, tqdm(pairs)))
+
+    out = pd.DataFrame({'mouse':[x[0] for x in pairs],
+                        'human':[x[1] for x in pairs],
+                        'similarity':sim})
+
+    return out
