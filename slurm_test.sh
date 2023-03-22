e0082f262b8a1630ce31ef8cef470d2e90493d62
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-03-22 15:26:20 -0400

    Test script for HPC

---
+++ slurm_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH -N 1 -c 8
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+
+export QBATCH_SYSTEM=slurm
+
+cd /hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
+
+source activate_hpfenv.sh
+
+python3 slurm_dev.py
