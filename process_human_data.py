f9edfd1e2abc0b41d8f931b5943a30368d8086e4
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-04-03 21:07:13 -0400

    Improved documentation and removed parallel option. Runs in parallel by default

--- process_human_data.py
+++ process_human_data.py
@@ -5,15 +5,20 @@
 # Created: February 22nd, 2023
 
 """
-Pipeline to process human neuroimaging data.
+Pipeline to process and cluster human neuroimaging data. 
 
 Description
 -----------
+The pipeline executes three main stages:
+1. Compute effect size images using absolute and relative Jacobian images
+2. Cluster individuals used similarity network fusion and spectral clustering
+3. Create cluster centroid images using absolute and relative effect size images
+
+The pipeline is mappable over multiple parameter sets.
 """
 
 # Packages -------------------------------------------------------------------
 
-import sys
 import argparse
 from pipelines import process_human_data
 from itertools import product
@@ -34,14 +39,19 @@ def parse_args():
         '--pipeline-dir',
         type=str,
         default='data/human/derivatives/',
-        help=("Path to pipeline directory.")
+        help=("Path to the directory in which to store pipeline outputs. "
+              "A sub-directory will be created based on the datasets specified "
+              "using --datasets. Directories for the various pipeline stages "
+              "will be created within this sub-directory.")
     )
 
     parser.add_argument(
         '--input-dir',
         type=str,
         default='data/human/registration/jacobians_resampled/',
-        help=("Path to Jacobians directory.")
+        help=("Path to the directory containing Jacobian images. The program "
+              "will look for a sub-directory 'resolution_##' using the value "
+              "passed to --resolution.")
     )
 
     parser.add_argument(
@@ -55,14 +65,14 @@ def parse_args():
         '--demographics',
         type=str,
         default='data/human/registration/DBM_input_demo_passedqc_wfile.csv',
-        help=("Path to CSV file containing demographics information.")
+        help="Path to file (.csv) containing demographics information."
     )
 
     parser.add_argument(
         '--mask',
         type=str,
         default='data/human/registration/reference_files/mask_3.0mm.mnc',
-        help=("Path to image mask")
+        help=("Path to mask file (.mnc) associated with the study images.")
     )
 
     parser.add_argument(
@@ -70,15 +80,7 @@ def parse_args():
         nargs='*',
         type=str,
         default=['POND', 'SickKids'],
-        help=("List of strings indicating datasets to include in processing.")
-    )
-
-    parser.add_argument(
-        '--parallel',
-        type=str,
-        default='true',
-        choices=['true', 'false'],
-        help=("Option to run in parallel.")
+        help=("List of strings indicating which datasets to include in processing.")
     )
 
     parser.add_argument(
@@ -87,14 +89,6 @@ def parse_args():
         help=("Number of processors to use in parallel.")
     )
 
-    # parser.add_argument(
-    #     '--verbose',
-    #     type=str,
-    #     default='true',
-    #     choices=['true', 'false'],
-    #     help='Verbosity.'
-    # )
-
     # Effect size arguments ---------------------------------------------------
     parser.add_argument(
         '--es-method',
@@ -102,7 +96,7 @@ def parse_args():
         type=str,
         default=['normative-growth'],
         choices=['normative-growth', 'propensity-matching'],
-        help=("Method to use to compute effect sizes.")
+        help=("Method to use to compute effect size images.")
     )
 
     parser.add_argument(
@@ -110,23 +104,20 @@ def parse_args():
         nargs='*',
         type=int,
         default=[3],
-        help=("Degrees of freedom hyperparameter in normative growth modelling.")
+        help=("The number of degrees of freedom for the natural splines used "
+              "in normative growth modelling. Ignored if --es-method is "
+             "'propensity-matching'.")
     )
 
     parser.add_argument(
-        '--es-combat',
-        nargs='*',
-        type=str,
-        default=['true'],
-        help=("Option to run ComBat normalization prior to normative growth modelling.")
-    )
-
-    parser.add_argument(
-        '--es-combat-batch',
+        '--es-batch',
         nargs='*',
         type=str,
         default=['Site-Scanner'],
-        help=("Batch variables to use in ComBat normalization.")
+        help=("Batch variables to use for normalization prior to normative "
+              "growth modelling. Variables must be found in --demographics. "
+              "Multiple batch variables must be specified in a single string, "
+              "separated by a hyphen (e.g. 'Site-Scanner').")
     )
 
     parser.add_argument(
@@ -134,14 +125,16 @@ def parse_args():
         nargs='*',
         type=int,
         default=[10],
-        help=("Number of controls to use for propensity-matching.")
+        help=("The number of controls to use for propensity-matching. "
+             "Ignored if --es-method is 'normative-growth'.")
     )
 
     parser.add_argument(
         '--es-matrix-file',
         type=str,
         default='effect_sizes.csv',
-        help=("Name of CSV file in which to store voxel-wise effect size matrices.")
+        help=("The basename of the file (.csv) in which to store voxel-wise "
+              "effect size matrices.")
     )
 
     # Clustering arguments ---------------------------------------------------
@@ -150,7 +143,9 @@ def parse_args():
         nargs=1,
         type=int,
         default=[10],
-        help=("Maximum number of clusters to identify in cluster solutions.")
+        help=("The maximum number of clusters to identify when clustering. "
+             "The program will obtain cluster solutions from 2 up to the "
+             "value provided.")
     )
 
     parser.add_argument(
@@ -158,7 +153,7 @@ def parse_args():
         nargs='*',
         type=str,
         default=['correlation'],
-        help=("Distance metric to use in similarity network fusion.")
+        help="The distance metric to use in similarity network fusion."
     )
 
     parser.add_argument(
@@ -166,7 +161,7 @@ def parse_args():
         nargs='*',
         type=int,
         default=[10],
-        help=("Number of nearest-neighbours to use in similarity network fusion.")
+        help=("The number of nearest-neighbours to use in similarity network fusion.")
     )
 
     parser.add_argument(
@@ -174,7 +169,7 @@ def parse_args():
         nargs='*',
         type=float,
         default=[0.5],
-        help=("Variance for the local model in similarity network fusion.")
+        help=("The variance for the local model in similarity network fusion.")
     )
 
     parser.add_argument(
@@ -182,21 +177,22 @@ def parse_args():
         nargs='*',
         type=int,
         default=[20],
-        help=("Number of iterations for the diffusion process in similarity network fusion.")
+        help=("The number of iterations for the diffusion process in similarity network fusion.")
     )
 
     parser.add_argument(
         '--cluster-file',
         type=str,
         default='clusters.csv',
-        help=("Name of the CSV file in which to store cluster assignments.")
+        help=("The basename of the file (.csv) in which to store cluster assignments.")
     )
 
     parser.add_argument(
         '--cluster-affinity-file',
         type=str,
         default='affinity.csv',
-        help=("Name of the file in which to store similarity network fusion affinity matrix.")
+        help=("The basename of the file (.csv) in which to store the affinity matrix "
+             "produced by similarity network fusion.")
     )
 
     # Cluster maps arguments ---------------------------------------------------
@@ -205,7 +201,7 @@ def parse_args():
         nargs='*',
         type=str,
         default=['mean'],
-        help=("Method to use to aggregate images within each cluster.")
+        help=("The method to use to compute cluster centroid images.")
     )
 
     args = vars(parser.parse_args())
@@ -217,8 +213,7 @@ def parse_args():
 if __name__ == '__main__':
 
     args = parse_args()
-    args['parallel'] = True if args['parallel'] == 'true' else False
-    # args['verbose'] = True if args['verbose'] == 'true' else False
+    args['datasets'] = tuple(args['datasets'])
     params = {key: val for key, val in args.items()
               if 'es_' in key or 'cluster_' in key}
     del params['es_matrix_file']
@@ -227,9 +222,7 @@ if __name__ == '__main__':
     param_keys = list(params.keys())
     for param_vals in product(*params.values()):
         param_set = dict(list(zip(param_keys, param_vals)))
-        param_set['es_combat'] = (True if param_set['es_combat'] == 'true'
-                                  else False)
-        param_set['es_combat_batch'] = param_set['es_combat_batch'].split('-')
+        param_set['es_batch'] = tuple(param_set['es_batch'].split('-'))
         param_msg = [': '.join([str(key), str(val)])
                      for key, val in param_set.items()]
         param_msg = reduce(lambda x, y: x + '\n\t' + y, param_msg)
