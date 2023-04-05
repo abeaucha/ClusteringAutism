4d02348bf94ab959b0407661ad5949e9529cb95e
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-04-05 12:11:57 -0400

    Script to create a partial mask

---
+++ partial_mask.R
@@ -0,0 +1,26 @@
+suppressPackageStartupMessages(library(RMINC))
+
+imgdir <- "data/human/registration/reference_files/"
+maskfile <- "mask_0.8mm.mnc"
+maskfile <- file.path(imgdir, maskfile)
+
+mask <- mincGetVolume(maskfile)
+mask <- floor(mask)
+mask_ind <- which(mask == 1)
+mask_size <- length(mask_ind)
+
+frac <- 0.1
+mask_frac <- ceiling(frac*mask_size)
+print(mask_frac)
+
+mask_partial <- numeric(length(mask))
+mask_partial[mask_ind[1:mask_frac]] <- 1
+attributes(mask_partial) <- attributes(mask)
+
+outfile <- paste0("mask_0.8mm_", frac, ".mnc")
+outfile <- file.path(imgdir, outfile)
+
+mincWriteVolume(buffer = mask_partial,
+                output.filename = outfile,
+                like.filename = maskfile,
+                clobber = TRUE)
