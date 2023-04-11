d103f9f200fbab3657c00e07ad30d08c8bad8174
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-04-11 12:50:56 -0400

    Similarity pipeline on graham

--- compute_cluster_similarity_graham.sh
+++ compute_cluster_similarity_graham.sh
@@ -12,9 +12,9 @@ source activate_venv.sh
 outdir=data/cross_species/v1/Models_135-POND_SickKids/similarity/
 
 python3 compute_cluster_similarity \
-	--pipeline-dir $SLURM_TMPDIR \
+	--pipeline-dir $outdir \
 	--mouse-cluster-dir data/mouse/derivatives/Models_135/cluster_maps/956/ \
-	--human-cluster-dir \
+	--human-cluster-dir data/human/derivatives/v1/POND_SickKids/cluster_maps/488-834-817/ \
 	--mouse-expr-dir data/mouse/expression/ \
 	--human-expr-dir data/human/expression/ \
 	--mouse-mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
@@ -25,26 +25,3 @@ python3 compute_cluster_similarity \
 	--gene-space average-latent-space \
 	--n-latent-spaces 5 \
 	--nproc $SLURM_CPUS_PER_TASK
-
-ls $SLURM_TMPDIR
-cp ${SLURM_TMPDIR}/* ${outdir}
-
-python3 process_human_data.py \
-  --pipeline-dir $SLURM_TMPDIR \
-  --input-dir data/human/registration/v1/jacobians_resampled/ \
-  --resolution 3.0 \
-  --demographics data/human/registration/v1/DBM_input_demo_passedqc_wfile.csv \
-  --mask data/human/registration/v1/reference_files/mask_3.0mm.mnc \
-  --datasets POND SickKids \
-  --nproc $SLURM_CPUS_PER_TASK \
-  --es-method normative-growth \
-  --es-df 3 \
-  --es-batch Site-Scanner \
-  --cluster-nk-max 10 \
-  --cluster-metric correlation \
-  --cluster-K 10 \
-  --cluster-sigma 0.5 \
-  --cluster-t 20 \
-  --cluster-map-method mean
-
-cp -r ${SLURM_TMPDIR}/* ${outdir}
