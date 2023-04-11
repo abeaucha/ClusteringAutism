d873858526cef0f369aaa4740eef587c3d30dfad
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-04-11 11:32:15 -0400

    Updated to 0.8mm

--- normative_growth_test.py
+++ normative_growth_test.py
@@ -1,9 +1,9 @@
 
 import processing
 
-imgdir = 'data/human/derivatives/POND_SickKids/jacobians/resolution_3.0/absolute/'
+imgdir = 'data/human/registration/v1/jacobians_resampled/resolution_0.8/absolute/'
 demographics = 'data/human/derivatives/POND_SickKids/DBM_input_demo_passedqc_wfile.csv'
-mask = 'data/human/registration/v1/reference_files/mask_3.0mm.mnc'
+mask = 'data/human/registration/v1/reference_files/mask_0.8mm.mnc'
 outdir = 'data/human/test/'
 key = 'file'
 df = 3
@@ -12,11 +12,11 @@ nbatches = 2
 nproc = 8
 
 tmp = processing.normative_growth_norm(imgdir = imgdir,
-                     demographics = demographics,
-                     mask = mask,
-                     outdir = outdir,
-                     key = key,
-                     df = df,
-                     batch = batch,
-                     nbatches = nbatches,
-                     nproc = nproc)
+                                       demographics = demographics,
+                                       mask = mask,
+                                       outdir = outdir,
+                                       key = key,
+                                       df = df,
+                                       batch = batch,
+                                       nbatches = nbatches,
+                                       nproc = nproc)
