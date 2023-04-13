59d5e3aee97df95953d7417d6a3b12e285281bcc
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-04-13 12:25:00 -0400

    Using 16 cores

--- process_human_data_graham_0.8mm.sh
+++ process_human_data_graham_0.8mm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=process_human_data
 #SBATCH -N 1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --time=8:00:00
 #SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
--- process_human_data_graham_3.0mm.sh
+++ process_human_data_graham_3.0mm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=process_human_data
 #SBATCH -N 1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
 #SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
