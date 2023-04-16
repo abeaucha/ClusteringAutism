b906f340b41639566ddb83ca19aab38473ed5681
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2023-04-16 10:55:41 -0400

    Cluster similarity scripts

--- slurm/compute_cluster_similarity_graham_v1_0.8mm.sh
+++ slurm/compute_cluster_similarity_graham_v1_0.8mm.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
-#SBATCH --output=logs/compute_cluster_similarity_v1_3.0mm_%j.out
+#SBATCH --output=logs/compute_cluster_similarity_v1_0.8mm_%j.out
 
 source activate_venv.sh
 
@@ -13,12 +13,12 @@ python3 compute_cluster_similarity.py \
 	--pipeline-dir data/cross_species/v1/ \
 	--human-pipeline-dir data/human/derivatives/v1/ \
 	--mouse-pipeline-dir data/mouse/derivatives/v2/ \
-	--human-params-id 547 \
+	--human-params-id 700 \
 	--mouse-params-id 107 \
 	--mouse-expr-dir data/mouse/expression/ \
 	--human-expr-dir data/human/expression/ \
 	--mouse-mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
-	--human-mask data/human/registration/v1/reference_files/mask_3.0mm.mnc \
+	--human-mask data/human/registration/v1/reference_files/mask_0.8mm.mnc \
 	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_studyspace_v1.csv \
 	--gene-space average-latent-space \
 	--n-latent-spaces 50 \
--- slurm/compute_cluster_similarity_graham_v2_3.0mm.sh
+++ slurm/compute_cluster_similarity_graham_v2_3.0mm.sh
@@ -5,21 +5,21 @@
 #SBATCH --mem=32G
 #SBATCH --time=48:00:00
 #SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
-#SBATCH --output=logs/compute_cluster_similarity_v1_3.0mm_%j.out
+#SBATCH --output=logs/compute_cluster_similarity_v2_3.0mm_%j.out
 
 source activate_venv.sh
 
 python3 compute_cluster_similarity.py \
-	--pipeline-dir data/cross_species/v1/ \
-	--human-pipeline-dir data/human/derivatives/v1/ \
+	--pipeline-dir data/cross_species/v2/ \
+	--human-pipeline-dir data/human/derivatives/v2/ \
 	--mouse-pipeline-dir data/mouse/derivatives/v2/ \
 	--human-params-id 547 \
 	--mouse-params-id 107 \
 	--mouse-expr-dir data/mouse/expression/ \
 	--human-expr-dir data/human/expression/ \
 	--mouse-mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
-	--human-mask data/human/registration/v1/reference_files/mask_3.0mm.mnc \
-	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_studyspace_v1.csv \
+	--human-mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
+	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_studyspace_v2.csv \
 	--gene-space average-latent-space \
 	--n-latent-spaces 50 \
 	--nproc $SLURM_CPUS_PER_TASK
