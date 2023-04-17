92ff2737686d1f1773a564d90d37575aa857f1cb
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-04-17 16:11:35 -0400

    Slurm scripts to test batching

--- slurm/process_human_data_test_1.sh
+++ slurm/process_human_data_test_1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=process_human_data
+#SBATCH --job-name=process_human_data_test
 #SBATCH -N 1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --time=1:00:00
 #SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
-#SBATCH --output=logs/process_human_data_v1_3.0mm_%j.out
+#SBATCH --output=logs/process_human_data_test_1_%j.out
 
 source activate_venv.sh
 
 ti=$(date +"%T")
 echo "Start time: $ti"
 python3 process_human_data.py \
-  --pipeline-dir data/human/derivatives/v1/ \
+  --pipeline-dir data/human/test/v1/nbatch_1/ \
   --input-dir data/human/registration/v1/jacobians_resampled/resolution_3.0/ \
   --demographics data/human/registration/v1/DBM_input_demo_passedqc_wfile.csv \
   --mask data/human/registration/v1/reference_files/mask_3.0mm.mnc \
