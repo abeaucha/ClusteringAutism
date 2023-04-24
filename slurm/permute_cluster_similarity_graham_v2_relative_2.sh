dcbbd358a928afd36df36f8f09b8c36517c8b273
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2023-04-24 14:39:11 -0400

    Updated slurm permutation scripts

---
+++ slurm/permute_cluster_similarity_graham_v2_relative_2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=permute_cluster_similarity_v2_rel_2
+#SBATCH -N 1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=48:00:00
+#SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
+#SBATCH --output=logs/permute_cluster_similarity_v2_0.8mm_rel_2_%j.out
+
+source activate_venv.sh
+
+ti=$(date)
+echo "Start time: $ti"
+python3 permute_cluster_similarity.py \
+	--pipeline-dir data/cross_species/v2/ \
+	--params-id 405 \
+	--human-pipeline-dir data/human/derivatives/v2/ \
+	--mouse-pipeline-dir data/mouse/derivatives/v2/ \
+	--mouse-expr-dir data/mouse/expression/ \
+	--human-expr-dir data/human/expression/ \
+	--mouse-mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
+	--human-mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_study_v2.csv \
+	--npermutations 2 \
+	--permutations-start 9 \
+	--jacobians relative \
+	--nproc $SLURM_CPUS_PER_TASK
+tf=$(date)
+echo "End time: $tf"
