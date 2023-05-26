0ef558968d0c815d9aaea3aaeafa997db0a226b4
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2023-05-26 10:46:17 -0400

    Updated permutations start

--- slurm/permute_cluster_similarity_graham_v2_absolute_1.sh
+++ slurm/permute_cluster_similarity_graham_v2_absolute_1.sh
@@ -22,7 +22,7 @@ python3 permute_cluster_similarity.py \
 	--human-mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
 	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_study_v2.csv \
 	--npermutations 4 \
-	--permutations-start 1 \
+	--permutations-start 13 \
 	--jacobians absolute \
 	--nproc $SLURM_CPUS_PER_TASK
 tf=$(date)
