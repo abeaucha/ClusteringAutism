76b7c0920db67d510efa394e1bd96a7a0613ac24
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-06-12 10:16:50 -0400

    Permutations start update

--- slurm/permute_cluster_similarity_graham_v2_relative_1.sh
+++ slurm/permute_cluster_similarity_graham_v2_relative_1.sh
@@ -22,8 +22,8 @@ python3 permute_cluster_similarity.py \
 	--mouse-mask data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 	--human-mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
 	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_study_v2.csv \
-	--npermutations 3 \
-	--permutations-start 50 \
+	--npermutations 4 \
+	--permutations-start 53 \
 	--jacobians relative \
 	--nproc $SLURM_CPUS_PER_TASK
 tf=$(date)
