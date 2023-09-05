b4ede488132d678f455b5bc693714606486f3eb2
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2023-09-05 11:03:24 -0400

    Updated job time

--- slurm/permute_cluster_similarity_lite_graham_v2.sh
+++ slurm/permute_cluster_similarity_lite_graham_v2.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --chdir=/project/def-jlerch/abeaucha/Paper_ClusteringAutism/main
 #SBATCH --output=logs/permute_cluster_similarity_lite_v2_0.8mm_%j.out
 
@@ -23,7 +23,7 @@ python3 permute_cluster_similarity_lite.py \
 	--human-microarray-coords data/human/expression/AHBA_microarray_coordinates_study_v2.csv \
 	--human-nk 2 \
 	--mouse-nk 4 \
-	--npermutations 4 \
+	--npermutations 50 \
 	--permutations-start 1 \
 	--nproc $SLURM_CPUS_PER_TASK
 tf=$(date)
