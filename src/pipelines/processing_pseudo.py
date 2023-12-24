9b2a0cf2ab3b86359796ed896a35a19fd794733c
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2023-12-23 18:24:47 -0700

    Flushed out some pseudo code for clustering and centroid modules

--- src/pipelines/processing_pseudo.py
+++ src/pipelines/processing_pseudo.py
@@ -25,11 +25,30 @@ def effect_sizes(...):
         # that computes the effect sizes for one set of Jacobians
         execute_slurm('compute_effect_sizes.R')
 
+    # This function also has to generate the effect size matrices.
+    # Is that separate from compute_effect_sizes.R?
+
 def clustering():
+    # This function clusters the absolute and relative effect sizes
+    # This is the part I'm unsure about. If I run a job array for the
+    # effect sizes, then I can submit this as a job with dependencies.
+    if environment is 'local':
+        execute_local('clustering.R')
+    if environment is 'slurm':
+        execute_slurm('clustering.R')
+        # This needs to get the jobs from the effect_sizes() module to use
+        # as dependencies
     return
 
 
 def centroids():
+    # This function generates the cluster centroid images
+    if environment is 'local':
+        execute_local('create_centroids.R')
+    if environment is 'slurm':
+        execute_slurm('create_centroids.R')
+        # This needs to get the job from the clustering() module to use
+        # as a dependency
     return
 
 
@@ -39,10 +58,14 @@ def execute_local(script, args):
     # command line args
     # 1. Determine if script is Python or R
     # 2. Deploy the appropriate subprocess
+    # If I make the scripts executable then I can just source them, no?
+    # I don't have to specify the interpreter
 
 def execute_slurm(script, args, args to map over?):
     # Submits a job array executing the script over the parameters
     # How do I submit a job array like this?
+    # This also needs to be able to submit a single job, rather than just a
+    # job array
 
 
 # What does compute_effect_sizes.R do?
