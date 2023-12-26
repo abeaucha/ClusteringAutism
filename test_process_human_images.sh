b38e882dc3281fde7f13ea26a2f654568af243b0
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2023-12-26 16:37:55 -0700

    Updated testing script resolution

--- test_process_human_images.sh
+++ test_process_human_images.sh
@@ -4,6 +4,6 @@
 
 process_human_images.py \
 --pipeline-dir data/human/derivatives/v2/ \
---input-dir data/human/registration/v2/jacobians_resampled/resolution_0.8/ \
+--input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
 --demographics data/human/registration/v2/subject_info/demographics.csv \
---mask data/human/registration/v2/reference_files/mask_0.8mm.mnc
+--mask data/human/registration/v2/reference_files/mask_3.0mm.mnc
