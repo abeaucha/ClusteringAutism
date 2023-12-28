926bbc895ac8984b6caaebf148f718c41706a1bf
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2023-12-28 18:10:05 -0500

    Playing with CLI args

--- test_process_human_images.sh
+++ test_process_human_images.sh
@@ -6,4 +6,6 @@ process_human_images.py \
 --pipeline-dir data/human/derivatives/v2/ \
 --input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
 --demographics data/human/registration/v2/subject_info/demographics.csv \
---mask data/human/registration/v2/reference_files/mask_3.0mm.mnc
+--mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
+--es-nbatches 2 \
+--nproc 4
