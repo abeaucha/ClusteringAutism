503f15aed8a6da5b1b2bbeb34cc7d5c86e575e6c
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-01-09 16:10:52 -0500

    Changed testing data dir

--- test_process_human_images.sh
+++ test_process_human_images.sh
@@ -3,7 +3,7 @@
 #source activate_venv.sh
 
 process_human_images.py \
---pipeline-dir data/human/derivatives/v2/ \
+--pipeline-dir data/test/human/derivatives/v2/ \
 --input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
 --demographics data/human/registration/v2/subject_info/demographics.csv \
 --mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
