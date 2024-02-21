e08bc3a2eaef30ed05a5024c49e21b79b8019342
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-02-21 16:46:06 -0500

    Using test mask

--- test_process_human_images.sh
+++ test_process_human_images.sh
@@ -18,10 +18,10 @@ process_human_images.py \
 --pipeline-dir data/test/human/derivatives/v2/ \
 --input-dir data/human/registration/v2/jacobians_resampled/resolution_0.8/ \
 --demographics data/human/registration/v2/subject_info/demographics.csv \
---mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+--mask data/human/registration/v2/reference_files/mask_0.8mm_test.mnc \
 --es-nbatches 1 \
 --execution slurm \
 --nproc 8 \
---slurm-njobs 100 \
+--slurm-njobs 32 \
 --slurm-time 30 \
 --slurm-mem 16G
