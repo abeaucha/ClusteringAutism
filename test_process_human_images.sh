1ce880c1adf0514616a04ac7044dc51448a1235a
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2024-02-21 20:37:38 -0500

    Testing 3.0mm

--- test_process_human_images.sh
+++ test_process_human_images.sh
@@ -9,26 +9,28 @@
 
 source activate_venv_hpc.sh
 
-#process_human_images.py \
-#--pipeline-dir data/test/human/derivatives/v2/ \
-#--input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
-#--demographics data/human/registration/v2/subject_info/demographics.csv \
-#--mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
-#--es-nbatches 1 \
-#--execution slurm \
-#--nproc 8 \
-#--slurm-njobs 32 \
-#--slurm-time 30 \
-#--slurm-mem 8G
-
 process_human_images.py \
 --pipeline-dir data/test/human/derivatives/v2/ \
---input-dir data/human/registration/v2/jacobians_resampled/resolution_0.8/ \
+--input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
 --demographics data/human/registration/v2/subject_info/demographics.csv \
---mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+--mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
 --es-nbatches 1 \
 --execution slurm \
 --nproc 8 \
---slurm-njobs 200 \
+--slurm-njobs 100 \
 --slurm-time 30 \
 --slurm-mem 16G
+
+#process_human_images.py \
+#--pipeline-dir data/test/human/derivatives/v2/ \
+#--input-dir data/human/registration/v2/jacobians_resampled/resolution_0.8/ \
+#--demographics data/human/registration/v2/subject_info/demographics.csv \
+#--mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+#--es-nbatches 1 \
+#--execution slurm \
+#--nproc 8 \
+#--slurm-njobs 200 \
+#--slurm-time 30 \
+#--slurm-mem 16G
+
+# 128GB seems to work. Runs pretty quickly with 200 jobs.
