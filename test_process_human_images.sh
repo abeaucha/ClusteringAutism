29596dacf676a49cf3684b0b8ed9f2b1232b30ac
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-02-23 17:51:13 -0500

    Testing pipeline at 3.0mm

--- test_process_human_images.sh
+++ test_process_human_images.sh
@@ -7,31 +7,28 @@
 #SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
 #SBATCH --output=logs/process_human_images_%j.out
 
-source activate_venv_hpc.sh
-
-#process_human_images.py \
-#--pipeline-dir data/test/human/derivatives/v2/ \
-#--input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
-#--demographics data/human/registration/v2/subject_info/demographics.csv \
-#--mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
-#--es-nbatches 1 \
-#--execution slurm \
-#--nproc 8 \
-#--slurm-njobs 100 \
-#--slurm-time 30 \
-#--slurm-mem 16G
+#source activate_venv_hpc.sh
 
 process_human_images.py \
 --pipeline-dir data/test/human/derivatives/v2/ \
---input-dir data/human/registration/v2/jacobians_resampled/resolution_0.8/ \
+--input-dir data/human/registration/v2/jacobians_resampled/resolution_3.0/ \
 --demographics data/human/registration/v2/subject_info/demographics.csv \
---mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+--mask data/human/registration/v2/reference_files/mask_3.0mm.mnc \
 --es-nbatches 1 \
---execution slurm \
---nproc 8 \
---slurm-njobs 300 \
---slurm-time 30 \
---slurm-mem 16G
+--execution local \
+--nproc 8
+
+#process_human_images.py \
+#--pipeline-dir data/test/human/derivatives/v2/ \
+#--input-dir data/human/registration/v2/jacobians_resampled/resolution_0.8/ \
+#--demographics data/human/registration/v2/subject_info/demographics.csv \
+#--mask data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+#--es-nbatches 1 \
+#--execution slurm \
+#--nproc 8 \
+#--slurm-njobs 300 \
+#--slurm-time 30 \
+#--slurm-mem 16G
 
 #process_human_images.py \
 #--pipeline-dir data/test/human/derivatives/v2/ \
