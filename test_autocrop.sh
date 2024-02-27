3c4801b60d8c84246048a060f7d79c4cac668a6a
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2024-02-26 19:42:50 -0500

    Script to test autocrop

---
+++ test_autocrop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+autocrop -quiet -clobber -isostep 3.0 \
+/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main/data/human/registration/v2/reference_files/mask_0.8mm.mnc \
+mask_test.mnc
