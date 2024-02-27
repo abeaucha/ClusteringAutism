7315634756dcf0c782885d3c508d7445a9257347
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2024-02-26 19:56:47 -0500

    Updated to test python script

--- test_autocrop.sh
+++ test_autocrop.sh
@@ -3,3 +3,8 @@
 autocrop -quiet -clobber -isostep 3.0 \
 /hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main/data/human/registration/v2/reference_files/mask_0.8mm.mnc \
 mask_test.mnc
+
+#python3 resample_images.py \
+#--imgdir test_input \
+#--outdir test_output \
+#--isostep 3.0
