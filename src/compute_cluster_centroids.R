a847d8a0bb95d66219f3f8498da8d633309b7e3c
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-02-27 13:24:01 -0500

    updated cluster centroid driver

--- src/compute_cluster_centroids.R
+++ src/compute_cluster_centroids.R
@@ -68,20 +68,39 @@ option_list <- list(
 ) 
 
 
+# Environment variables ------------------------------------------------------
+
+SRCPATH <- Sys.getenv("SRCPATH")
+
+
+# Functions ------------------------------------------------------------------
+
+source(file.path(SRCPATH, "processing.R"))
+
+
 # Main -----------------------------------------------------------------------
 
-#Parse command line args
+# Parse command line args
 args <- parse_args(OptionParser(option_list = option_list))
+
+# TODO remove lines when script works
+# REMOVE THESE LINES WHEN FINISHED
+args[["imgdir"]] <- "data/test/human/derivatives/v2/547/effect_sizes/resolution_3.0/absolute/"
+args[["cluster-file"]] <- "data/test/human/derivatives/v2/547/clusters/resolution_3.0/clusters.csv"
+args[["mask"]] <- "data/human/registration/v2/reference_files/mask_3.0mm.mnc"
+args[["outdir"]] <- "data/test/human/derivatives/v2/547/centroids/resolution_3.0/absolute/"
+args[["method"]] <- "mean"
+args[["execution"]] <- "local"
+args[["nproc"]] <- 8
+
 imgdir <- args[["imgdir"]]
 clusterfile <- args[["cluster-file"]]
 mask <- args[["mask"]]
 outdir <- args[["outdir"]]
 method <- args[["method"]]
+execution <- args[["execution"]]
 nproc <- args[["nproc"]]
 verbose <- ifelse(args[["verbose"]] == "true", TRUE, FALSE)
-
-print(args)
-quit()
 
 # Check required arguments
 args_req <- c("cluster-file", "imgdir", "mask", "outdir")
@@ -94,7 +113,15 @@ for (arg in args_req) {
 
 # Check that cluster file is a CSV
 if (tools::file_ext(clusterfile) != "csv") {
-  stop("Clusters file must be a CSV.")
+  stop(paste("Clusters file must be a CSV:",
+             clusterfile))
+}
+
+# Check that imgdir contains images
+imgfiles <- list.files(imgdir, full.names = TRUE, pattern = "*.mnc")
+if (length(imgfiles) == 0) {
+  stop(paste("No MINC files were found in the image directory:",
+             imgdir))
 }
 
 # Create outdir if needed
@@ -102,56 +129,38 @@ if (!dir.exists(outdir)) {
   dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 }
 
+# Check execution option
+if (execution == "local") {
+  resources <- list() 
+} else if (execution == "slurm") {
+  njobs <- args[["slurm-njobs"]]
+  resources <- list(memory = args[["slurm-mem"]],
+                    walltime = args[["slurm-time"]]*60)
+} else {
+  stop()
+}
+
 # Import cluster information
 if (verbose) {message("Importing cluster information...")}
-df_clusters <- data.table::fread(clusterfile, header = TRUE) %>% 
-  as_tibble() %>% 
-  column_to_rownames("ID")
+df_clusters <- as_tibble(data.table::fread(clusterfile, header = TRUE)) %>% 
+  rename(file = ID) %>% 
+  mutate(file = file.path(imgdir, file))
 
-# Create centroids
+
+# Create centroid images for all clusters
 if (verbose) {message("Creating centroid images...")}
 sink(nullfile(), type = "output")
-imgfiles <- list.files(imgdir, full.names = TRUE, pattern = "*.mnc")
-for (j in 1:ncol(df_clusters)) {
-  
-  krange <- sort(unique(df_clusters[,j]))
-  for (k in krange) {
-    
-    if (verbose) {
-      message(paste("nk =", max(krange), ":", "k =", k))
-    }
-    
-    rows_k <- df_clusters[,j] == k
-    id_k <- rownames(df_clusters)[rows_k]
-    files_k <- file.path(imgdir, id_k)
-    file_exists <- files_k %in% imgfiles
-    if (sum(!file_exists) != 0) {
-      warning(paste(sum(!(file_exists)), "files not found in directory", 
-                    imgdir, "\nThese files will be ommitted from",
-                    "the calculation."))
-    }
-    
-    # Centroid function
-    if (method == "mean") {
-      centroid <- mean
-    } else if (method == "median") {
-      centroid <- median
-    } else {
-      stop("Argument --method must be one of {mean, median}.")
-    }
-    
-    # Create centroid image
-    cluster_map <- mcMincApply(filenames = files_k,
-                               fun = centroid,
-                               mask = mask,
-                               cores = nproc)
-    
-    # Export image
-    outfile <- paste0("cluster_map_nk_", max(krange), "_k_", k, ".mnc")
-    outfile <- file.path(outdir, outfile)
-    mincWriteVolume(cluster_map,
-                    output.filename = outfile,
-                    clobber = TRUE)
-  }
+cols_iter <- colnames(df_clusters)[colnames(df_clusters) != "file"]
+for (col in cols_iter) {
+  clusters <- df_clusters[, c("file", col)]
+  colnames(clusters) <- c("file", "k")
+  centroids <- compute_cluster_centroids(clusters = clusters,
+                                         mask = mask,
+                                         outdir = outdir,
+                                         method = method,
+                                         execution = execution,
+                                         nproc = nproc,
+                                         njobs = njobs,
+                                         resources = resources)
 }
 sink(NULL)
