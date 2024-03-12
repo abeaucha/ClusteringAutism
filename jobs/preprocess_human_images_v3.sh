571f24578c500ec29dc5ba9b63368bc930fc41b3
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-03-12 13:48:33 -0400

    Updated ncpus

--- jobs/preprocess_human_images_v3.sh
+++ jobs/preprocess_human_images_v3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=preprocess_human_images_v3
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
 #SBATCH --time=12:00:00
 #SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
