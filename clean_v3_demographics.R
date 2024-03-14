779a5e80f0e3d720e1219bb41dd3f3b31c52cf70
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-03-14 14:38:30 -0400

    Cleaned up v3 demographics info

--- clean_v3_demographics.R
+++ clean_v3_demographics.R
@@ -19,56 +19,74 @@ suppressPackageStartupMessages(library(tidyverse))
 ## Directories ---------------------------------------------------------------
 
 # Registration base directories
-registration_dir_v2 <- "data/human/registration/v2/"
-registration_dir_v3 <- "data/human/registration/v3/"
+registration_dir <- "data/human/registration/v3/"
 
 # Jacobian image directories
-imgdir_v2 <- file.path(registration_dir_v2, "jacobians")
-imgdir_v3 <- file.path(registration_dir_v3, "jacobians")
+imgdir <- file.path(registration_dir, "jacobians")
 
 # Demographics directories
-demographics_dir_v2 <- file.path(registration_dir_v2, "subject_info/")
-demographics_dir_v3 <- file.path(registration_dir_v3, "subject_info/")
+demographics_dir <- file.path(registration_dir, "subject_info/")
 
 # File suffix for the Jacobian images
 file_suffix <- ".denoise_fwhm_2mm.nii.gz"
 
 # Images files and IDs
 jacobians <- "absolute"
-imgfiles <- imgdir_v3 %>% 
+imgfiles <- imgdir %>% 
   file.path(jacobians, "smooth") %>% 
   list.files(pattern = "*.nii.gz")
 
 # Create a data frame with image files and IDs
-df_imgs <- tibble(file = imgfiles) %>% 
+imgs <- tibble(file = imgfiles) %>% 
   mutate(Scan_ID = file %>% 
            str_remove(file_suffix))
 
 
-
 ## SickKids data -------------------------------------------------------------
-
-demographics_v2 <- demographics_dir_v2 %>% 
-  file.path("demographics.csv") %>% 
-  read_csv(show_col_types = FALSE)
 
 # Clean up SickKids demographics
 sickkids <- "Sickkids_dbm_input_demo_n500.csv"
-sickkids <- file.path(demographics_dir_v3, "SickKids", sickkids)
+sickkids <- file.path(demographics_dir, "SickKids", sickkids)
 sickkids <- read_csv(sickkids, show_col_types = FALSE) %>% 
   select(Subject_ID, Scan_ID,
          Rating_Motion, Rating_IN3, QC_Status,
          Site, Scanner,
          Age, DX, Sex) %>%
   mutate(Dataset = "SickKids",
-         file = str_c(Subject_ID, file_suffix))
+         file = str_c(Subject_ID, file_suffix)) %>% 
+  select(-Rating_Motion, -Rating_IN3, -QC_Status)
 
 
 ## POND data -----------------------------------------------------------------
 
+# Import POND neuroanatomy export
+# pond_neuro <- "pond-neuroanatomy-20240212.csv"
+pond_neuro <- "pond-neuroanatomy-20230111.csv"
+pond_neuro <- file.path(demographics_dir, "POND", pond_neuro)
+pond_neuro <- read_csv(pond_neuro, show_col_types = FALSE) %>% 
+  select(Subject_ID = subject, Scan_ID = scan,
+         Scanner = scanned_on, Age = age_at_scan) 
+
+# POND site and scanner information
+pond_sites <- tibble(Scanner = c("SickKids_Trio", "SickKids_Prisma", 
+                                 "Queens_Trio", "Queens_Prisma", 
+                                 "Bloorview_Prisma"),
+                     Site = c("Toronto", "Toronto", 
+                              "Queens", "Queens", 
+                              "Bloorview"))
+
+# Update site and scanner info
+pond_neuro <- pond_neuro %>%   
+  left_join(pond_sites, by = "Scanner")
+
+# Join POND neuro export to v3 imgs 
+pond <- pond_neuro %>% 
+  inner_join(imgs, by = "Scan_ID")
+
+# Import POND metadata
 pond_metadata <- "pond-metadata-20240110.csv"
-pond_metadata <- file.path(demographics_dir_v3, "POND", pond_metadata)
-pond_metadata <- read_csv(pond_metadata) %>% 
+pond_metadata <- file.path(demographics_dir, "POND", pond_metadata)
+pond_metadata <- read_csv(pond_metadata, show_col_types = FALSE) %>% 
   distinct() %>% 
   select(Subject_ID = SUBJECT,
          Sex = NSI_SEX,
@@ -81,40 +99,16 @@ pond_metadata <- read_csv(pond_metadata) %>%
          DX = ifelse(DX == "Typically Developing", "Control", DX),
          DX = ifelse(DX == "Unaffected Sibling", "Control", DX))
 
-# POND site and scanner information
-pond_sites <- tibble(Scanner = c("SickKids_Trio", "SickKids_Prisma", 
-                                 "Queens_Trio", "Queens_Prisma", 
-                                 "Bloorview_Prisma"),
-                     Site = c("Toronto", "Toronto", 
-                              "Queens", "Queens", 
-                              "Bloorview"))
-
-pond_neuro <- "pond-neuroanatomy-20240212.csv"
-pond_neuro <- file.path(demographics_dir_v3, "POND", pond_neuro)
-pond_neuro <- read_csv(pond_neuro) %>% 
-  select(Subject_ID = subject, Scan_ID = scan,
-         Scanner = scanned_on, Age = age_at_scan) %>% 
-  left_join(pond_sites, by = "Scanner")
-
-pond <- pond_neuro %>% 
-  inner_join(df_imgs, by = "Scan_ID") %>% 
-  left_join(pond_metadata, by = "Subject_ID")
-
-
-# %>% 
-filter(str_detect(Subject_ID, "^N")) %>% 
-  mutate(Subject_ID = str_remove(Subject_ID, "_.*"))
-
-# Join image files to basic demographics info
-hbn_imgs <- hbn_imgs %>% 
-  left_join(hbn_basic, by = "Subject_ID") 
-
+# Combine imaging and demographics information
+pond <- pond %>% 
+  left_join(pond_metadata, by = "Subject_ID") %>% 
+  mutate(Dataset = "POND")
 
 
 ## HBN data ------------------------------------------------------------------
 
 # HBN demographics directory
-hbn_dir <- file.path(demographics_dir_v3, "HBN")
+hbn_dir <- file.path(demographics_dir, "HBN")
 
 # Files containing basic demographics information (e.g. age, sex)
 hbn_basic_files <- c("MRI_CBIC_participants.csv", 
@@ -129,65 +123,124 @@ hbn_basic <- hbn_basic_files %>%
   map(function(x){
     x %>%
       select(Subject_ID = participant_id,
-             Release = release_number,
              Sex, Age)
   }) %>% 
   bind_rows(.id = "Site") %>% 
   mutate(Sex = ifelse(Sex == 1, "Female", "Male"))
 
-
 # Create a data frame with image files and IDs
-hbn_imgs <- tibble(file = imgfiles) %>% 
-  mutate(Subject_ID = file %>% 
-           str_remove(file_suffix) %>% 
-           str_remove("sub-")) %>% 
-  filter(str_detect(Subject_ID, "^N")) %>% 
-  mutate(Subject_ID = str_remove(Subject_ID, "_.*"))
+hbn_imgs <- imgs %>% 
+  filter(str_starts(file, "sub-N")) %>% 
+  mutate(Subject_ID = Scan_ID %>% 
+           str_remove("sub-") %>% 
+           str_remove("_.*"))
 
 # Join image files to basic demographics info
 hbn_imgs <- hbn_imgs %>% 
   left_join(hbn_basic, by = "Subject_ID") 
 
+# Import HBN diagnostic information
+hbn_dx <- "9994_ConsensusDx_20220728.csv"
+hbn_dx <- file.path(hbn_dir, "assessment_data", hbn_dx)
+hbn_dx <- read_csv(hbn_dx, show_col_types = FALSE)
+hbn_dx <- hbn_dx[2:nrow(hbn_dx),]
+
+# Disorder groups to include
+hbn_dx_include <- c("Neurodevelopmental Disorders",
+                    "Obsessive Compulsive and Related Disorders",
+                    "No Diagnosis Given")
+
+# Neurodevelopmental disorders to exclude
+hbn_dx_exclude <- c("Language Disorder",
+                    "Specific Learning Disorder with Impairment in Mathematics",
+                    "Specific Learning Disorder with Impairment in Reading",
+                    "Specific Learning Disorder with Impairment in Written Expression",
+                    "Speech Sound Disorder",
+                    "Social (Pragmatic) Communication Disorder",
+                    "Other Specified Neurodevelopmental Disorder",
+                    "Unspecified Neurodevelopmental Disorder",
+                    "Excoriation (Skin-Picking) Disorder")
+
+# Select DX columns and rows
+hbn_dx <- hbn_dx %>% 
+  select(Subject_ID = EID, 
+         DX_01_Cat,
+         DX_01) %>% 
+  filter(DX_01_Cat %in% hbn_dx_include,
+         !(DX_01 %in% hbn_dx_exclude)) %>% 
+  arrange(DX_01_Cat, DX_01)
+
+# Export full set of DX information
+outfile <- "HBN_DX_categories.csv"
+outfile <- file.path(hbn_dir, outfile)
+write_csv(x = hbn_dx, file = outfile)
+
+# Combine some DX using new labels
+hbn_dx_labels <- tibble(DX_01 = c("ADHD-Combined Type", 
+                                  "ADHD-Hyperactive/Impulsive Type",
+                                  "ADHD-Inattentive Type",
+                                  "Autism Spectrum Disorder",
+                                  "Intellectual Disability-Mild",
+                                  "Intellectual Disability-Moderate",
+                                  "Intellectual Disability-Severe",
+                                  "Other Specified Attention-Deficit/Hyperactivity Disorder", 
+                                  "Other Specified Tic Disorder", 
+                                  "Persistent (Chronic) Motor or Vocal Tic Disorder",
+                                  "Provisional Tic Disorder",
+                                  "Tourettes Disorder",
+                                  "Unspecified Attention-Deficit/Hyperactivity Disorder",
+                                  "Obsessive-Compulsive Disorder"),
+                         DX = c("ADHD", 
+                                "ADHD",
+                                "ADHD",
+                                "ASD",
+                                "ID",
+                                "ID",
+                                "ID",
+                                "ADHD",
+                                "Tourette Syndrome",
+                                "Tourette Syndrome",
+                                "Tourette Syndrome",
+                                "Tourette Syndrome",
+                                "ADHD", 
+                                "OCD"))
+
+# Join new labels to DX data frame
+hbn_dx <- hbn_dx %>% 
+  left_join(hbn_dx_labels, by = "DX_01") %>% 
+  filter(!is.na(DX)) %>% 
+  select(Subject_ID, DX)
+
+# Join image and diagnostic data frames
+hbn <- left_join(hbn_imgs,
+                 hbn_dx,
+                 by = "Subject_ID") %>% 
+  mutate(Dataset = "HBN",
+         Scanner = "")
 
 
+## Combine data --------------------------------------------------------------
 
+# Combine demographics data frames
+demographics <- bind_rows(pond, sickkids, hbn)
 
-# 
-# 
-# hbn_dx <- "9994_ConsensusDx_20220728.csv"
-# hbn_dx <- file.path(demographics_dir, "HBN", "assessment_data", hbn_dx)
-# hbn_dx <- read_csv(hbn_dx)
-# 
-# hbn_dx_mv <- "9994_ConsensusDx_20220728_MV.csv"
-# hbn_dx_mv <- file.path(demographics_dir, "HBN", "assessment_data", hbn_dx_mv)
-# hbn_dx_mv <- read_csv(hbn_dx_mv)
-# 
-# 
-# hbn_dx %>% 
-#   select(DX_01_Cat, DX_01_Sub, DX_01) %>% 
-#   distinct() %>% 
-#   arrange(DX_01_Cat) %>% 
-#   View()
-# 
-# hbn_dx %>% 
-#   filter(DX_01_Cat == "Neurodevelopmental Disorders") %>% 
-#   nrow()
-# 
-# hbn_dx %>% 
-#   filter(DX_02_Cat == "Neurodevelopmental Disorders") %>% 
-#   nrow()
-# 
-# hbn_dx %>% 
-#   filter(DX_03_Cat == "Neurodevelopmental Disorders") %>% 
-#   nrow()
-# 
-# hbn_dx %>% 
-#   filter(DX_01_Cat == "Neurodevelopmental Disorders",
-#          DX_02_Cat == "Neurodevelopmental Disorders") %>% 
-#   select(DX_01_Cat, DX_01_Sub, DX_01, DX_02_Cat, DX_02_Sub, DX_02) %>% 
-#   distinct() %>% 
-#   View()
-# 
-# 
-# hbn_dx %>% 
-#   filter(is.na(DX_01_Cat))
+# Remove participants with missing demographics
+demographics <- demographics %>% 
+  filter(!is.na(DX), 
+         !is.na(Sex),
+         !is.na(Age),
+         !is.na(Site),
+         !is.na(Scanner))
+
+# Arrange columns and rows
+demographics <- demographics %>% 
+  select(Subject_ID, Scan_ID, Dataset,
+         Age, Sex, DX, 
+         Site, Scanner,
+         file) %>% 
+arrange(Dataset, Subject_ID)
+
+# Export
+outfile <- "demographics.csv"
+outfile <- file.path(demographics_dir, outfile)
+write_csv(x = demographics, file = outfile)
