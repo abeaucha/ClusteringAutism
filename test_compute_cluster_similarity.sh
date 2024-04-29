0496067259672299bf6eed6b7eb5f19a62ad5578
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-04-29 17:20:49 -0400

    Updated pipeline params for testing

--- test_compute_cluster_similarity.sh
+++ test_compute_cluster_similarity.sh
@@ -9,11 +9,10 @@ compute_cluster_similarity.py \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
 --gene-space average-latent-space \
---n-latent-spaces 50 \
---nproc 8 \
+--n-latent-spaces 10 \
 --jacobians absolute relative \
 --execution slurm \
 --registry-cleanup false \
---slurm-njobs 2 \
---slurm-mem 32G \
+--slurm-njobs 10 \
+--slurm-mem 16G \
 --slurm-time 2:00:00
