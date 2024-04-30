fd757476865e0d51329168dadcc2f3f5dcee74f3
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-04-30 13:24:09 -0400

    Slurm job file for similarity pipeline

---
+++ jobs/compute_cluster_similarity_v3_POND_SickKids.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=compute_cluster_similarity
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --time=00:30:00
+#SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
+#SBATCH --output=logs/compute_cluster_similarity_v3_POND_SK_%j.out
+
+# Activate virtual environment
+source activate_venv_hpc.sh
+
+# Pipeline registry directory
+REGISTRY="compute_cluster_similarity_registry_${SLURM_JOB_ID}"
+
+# Execute pipeline
+compute_cluster_similarity.py \
+--pipeline-dir data/test/cross_species/v3/ \
+--species human mouse \
+--input-dirs data/human/derivatives/v3/ data/mouse/derivatives/v3/ \
+--param-ids 700 107 \
+--expr-dirs data/human/expression data/mouse/expression \
+--masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
+--microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
+--gene-space average-latent-space \
+--n-latent-spaces 100 \
+--jacobians absolute relative \
+--execution slurm \
+--registry-name $REGISTRY \
+--registry-cleanup false \
+--slurm-njobs 300 \
+--slurm-mem 16G \
+--slurm-time 6:00:00
