b5eade02459198799d1155e20d52eeecb2fca77d
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-05-13 14:22:51 -0400

    Potential fix to microarray bug

--- jobs/prepare_microarray_data_v3.sh
+++ jobs/prepare_microarray_data_v3.sh
@@ -14,11 +14,17 @@
 source activate_venv.sh
 
 # Execute pipeline
+# prepare_microarray_data.py \
+#   --pipeline-dir data/human/expression/v3/ \
+#   --transforms \
+#   [data/human/registration/v3/transforms/MNI_09c_0.5mm_to_MNI_09b_hires_0GenericAffine.mat,1] \
+#   data/human/registration/v3/transforms/MNI_09c_0.5mm_to_MNI_09b_hires_1InverseWarp.nii.gz \
+#   --template data/human/registration/v3/reference_files/model_0.8mm.mnc
 prepare_microarray_data.py \
   --pipeline-dir data/human/expression/v3/ \
   --transforms \
-  [data/human/registration/v3/transforms/MNI_09c_0.5mm_to_MNI_09b_hires_0GenericAffine.mat,1] \
   data/human/registration/v3/transforms/MNI_09c_0.5mm_to_MNI_09b_hires_1InverseWarp.nii.gz \
+  [data/human/registration/v3/transforms/MNI_09c_0.5mm_to_MNI_09b_hires_0GenericAffine.mat,1] \
   --template data/human/registration/v3/reference_files/model_0.8mm.mnc
 
 deactivate
