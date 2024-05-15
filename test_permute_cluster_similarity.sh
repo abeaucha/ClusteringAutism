59a88b6bee962ea74072dcfe2961635e582fc688
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-05-15 16:02:20 -0400

    Script to test permutations

--- test_permute_cluster_similarity.sh
+++ test_permute_cluster_similarity.sh
@@ -7,5 +7,13 @@ permute_cluster_similarity.py \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---permutations-n 1
+--permutations-n 2 \
+--permutations-start 1 \
+--off-diagonal 2 \
+--execution slurm \
+--registry-name permutation_test \
+--registry-cleanup false \
+--slurm-njobs 2 \
+--slurm-mem 16G \
+--slurm-time 8:00:00
 
