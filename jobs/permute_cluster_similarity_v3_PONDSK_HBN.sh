c17a82c8212e9b33ac62051174c9652e4eea1f5a
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-06-09 14:06:04 -0400

    Updated jobs

--- jobs/permute_cluster_similarity_v3_PONDSK_HBN.sh
+++ jobs/permute_cluster_similarity_v3_PONDSK_HBN.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=72:00:00
 #SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
 #SBATCH --output=logs/permute_cluster_similarity_v3_PONDSK_HBN_%j.out
-##SBATCH --dependency=afterok:
+#SBATCH --dependency=afterok:10570276
 ##SBATCH --qos=abeauchamp_q
 
 # Activate virtual environment
