4a231862a5d88f14da0f1738ef53f134669d4cf7
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-09-04 11:26:11 -0400

    Permutations update

--- jobs/permute_cluster_similarity_v3_PONDSK_MICe.sh
+++ jobs/permute_cluster_similarity_v3_PONDSK_MICe.sh
@@ -23,7 +23,7 @@ permute_cluster_similarity.py \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---permutations-start 231 \
+--permutations-start 241 \
 --permutations-n 10 \
 --off-diagonal 1 \
 --execution slurm \
