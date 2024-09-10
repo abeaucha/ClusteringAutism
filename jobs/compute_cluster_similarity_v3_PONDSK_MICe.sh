8af276975d0c558029f56e524637b2cb4ad61978
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2024-09-10 19:25:27 -0400

    Permutations update

--- jobs/compute_cluster_similarity_v3_PONDSK_MICe.sh
+++ jobs/compute_cluster_similarity_v3_PONDSK_MICe.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
 #SBATCH --output=logs/compute_cluster_similarity_v3_POND_SK_%j.out
-#SBATCH --dependency=afterok:10503819
+##SBATCH --dependency=afterok:
 
 # Activate virtual environment
 source activate_venv_hpc.sh
