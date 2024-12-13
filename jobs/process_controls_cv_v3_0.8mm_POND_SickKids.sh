bbe614ad68cf443e5e7107dfd59be8769d2e4d85
abeaucha <antoine.beauchamp@mail.utoronto.ca>
2024-12-13 13:31:24 -0500

    Job script to run control cv pipeline

---
+++ jobs/process_controls_cv_v3_0.8mm_POND_SickKids.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=controls_cv
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --time=12:00:00
+#SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
+#SBATCH --output=logs/process_controls_cv_v3_0.8mm_POND_SK_%j.out
+##SBATCH --dependency=afterok:
+
+# This pipeline ran in ___ minutes with --time=12:00:00.
+# Should be able to run it with a shorter walltime.
+
+# Activate virtual environment
+source activate_venv_hpc.sh
+
+# Pipeline registry directory
+REGISTRY="process_controls_cv_registry_${SLURM_JOB_ID}"
+
+# What node is executing?
+echo $SLURM_NODELIST
+
+# Execute pipeline
+process_control_images_resampled.py \
+--pipeline-dir data/human/derivatives/v3/ \
+--input-dir data/human/registration/v3/jacobians_resampled/resolution_0.8/ \
+--demographics data/human/registration/v3/subject_info/demographics.csv \
+--mask data/human/registration/v3/reference_files/mask_0.8mm.mnc \
+--datasets POND SickKids \
+--cv-n 5 \
+--cv-start 1 \
+--es-method normative-growth \
+--es-group controls \
+--es-df 3 \
+--cluster-resolution 3.0 \
+--execution slurm \
+--nproc 8 \
+--registry-name $REGISTRY \
+--registry-cleanup false \
+--slurm-njobs 300 \
+--slurm-time 120 \
+--slurm-mem 16G
+
+deactivate
