99b7d3973c310d896b983fb85d943bcaffea08b9
Antoine Beauchamp <antoinebeauchamp92@gmail.com>
2025-01-08 12:12:56 -0500

    Procrustes analysis

---
+++ procrustes_dev.py
@@ -0,0 +1,21 @@
+
+cv_dir = 'data/human/derivatives/v3/916/cross_validation/'
+
+target_dir = 'data/human/derivatives/v3/916/cross_validation/sample_1/centroids/resolution_0.8/'
+
+jacobians = ('absolute', 'relative')
+
+# for j in jacobians:
+
+# Import centroids 1 and 2 for given jacobians
+target_imgs = ...
+target_imgs = import_images(...)
+
+# for i in samples-1:
+
+# Get input dir for sample i and Jacobians j
+input_dir = 'data/human/derivatives/v3/916/cross_validation/sample_i/centroids/resolution_0.8/absolute/'
+
+# Get centroids 1 and 2
+input_imgs = ...
+
