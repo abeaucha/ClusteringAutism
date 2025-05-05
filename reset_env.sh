131c81a0b68ec45b86e938da2c4ac543041403c5
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-05-05 11:46:46 -0400

    Removed reset_env.sh from repo

--- reset_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# Make conda tools visible
-source $(conda info --base)/etc/profile.d/conda.sh
-
-conda remove --name clustering-autism-minc-env --all -y
-conda remove --name clustering-autism-env --all -y
-conda clean --all -y
