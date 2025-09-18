8210dcc78e3b648fe15dea5bdb5a41b68b503eaa
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-09-18 16:05:10 -0400

    Updated job scripts

--- jobs/local/compute_cluster_similarity_PONDSK_MICe_test.sh
+++ jobs/local/compute_cluster_similarity_PONDSK_MICe_test.sh
@@ -4,11 +4,10 @@ compute_cluster_similarity.py \
 --pipeline-dir data/cross_species/v3/ \
 --species human mouse \
 --input-dirs data/human/derivatives/v3/ data/mouse/derivatives/v3/ \
---param-ids 013 107 \
+--param-ids 700 107 \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---gene-space avg-mlp-latent-space \
---n-latent-spaces 50 \
+--gene-space mlp-latent-space \
 --jacobians absolute relative \
 --nproc 8
