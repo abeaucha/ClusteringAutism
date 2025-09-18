d2e5799a9c2c38610ce23ca1ebdcc157defedb4a
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-09-18 17:32:37 -0400

    Fixed arg bug

--- jobs/SLURM/compute_cluster_similarity_v3_PONDSK_HBN.sh
+++ jobs/SLURM/compute_cluster_similarity_v3_PONDSK_HBN.sh
@@ -20,7 +20,7 @@ compute_cluster_similarity.py \
 --pipeline-dir data/cross_species/v3/ \
 --species human human \
 --input-dirs data/human/derivatives/v3/ data/human/derivatives/v3/ \
---param-ids 700 013 \
+--input-params-ids 700 013 \
 --expr-dirs data/human/expression data/human/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/human/registration/v3/reference_files/mask_0.8mm.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
--- jobs/SLURM/compute_cluster_similarity_v3_PONDSK_MICe.sh
+++ jobs/SLURM/compute_cluster_similarity_v3_PONDSK_MICe.sh
@@ -20,7 +20,7 @@ compute_cluster_similarity.py \
 --pipeline-dir data/cross_species/v3/ \
 --species human mouse \
 --input-dirs data/human/derivatives/v3/ data/mouse/derivatives/v3/ \
---param-ids 700 107 \
+--input-params-ids 700 107 \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
