442daca13ee5812e3dffd509d4b970a7e68696e9
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-09-29 11:19:24 -0400

    Updated job to use VAE embedding

--- jobs/SLURM/compute_cluster_similarity_v3_PONDSK_MICe.sh
+++ jobs/SLURM/compute_cluster_similarity_v3_PONDSK_MICe.sh
@@ -24,7 +24,7 @@ compute_cluster_similarity.py \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---gene-space mlp-latent-space \
+--gene-space vae-latent-space \
 --jacobians absolute relative \
 --execution slurm \
 --nproc 600 \
