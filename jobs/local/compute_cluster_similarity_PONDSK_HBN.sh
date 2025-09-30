28c17552809c3e2eb16fd99e8bbc348b78f4db24
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-09-30 11:57:11 -0400

    Updated jobs scripts for VAE

--- jobs/local/compute_cluster_similarity_PONDSK_HBN.sh
+++ jobs/local/compute_cluster_similarity_PONDSK_HBN.sh
@@ -8,7 +8,6 @@ compute_cluster_similarity.py \
 --expr-dirs data/human/expression data/human/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/human/registration/v3/reference_files/mask_0.8mm.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---gene-space avg-mlp-latent-space \
---n-latent-spaces 50 \
+--gene-space vae-latent-space \
 --jacobians absolute relative \
---nproc 8
+--nproc 36
