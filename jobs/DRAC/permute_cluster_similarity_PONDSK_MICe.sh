40c2c4478ddf63321093a807db724f65d9315202
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-10-01 17:01:06 -0400

    Trying on 2 nodes

--- jobs/DRAC/permute_cluster_similarity_PONDSK_MICe.sh
+++ jobs/DRAC/permute_cluster_similarity_PONDSK_MICe.sh
@@ -17,7 +17,7 @@ permute_cluster_similarity.py \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
 --permutations-start 1 \
---permutations-n 500 \
+--permutations-n 100 \
 --off-diagonal 1 \
 --execution local \
 --nproc 384
