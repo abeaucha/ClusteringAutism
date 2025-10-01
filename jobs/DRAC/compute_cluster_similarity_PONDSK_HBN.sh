2ee3110a0241ae3d16a3c2ee7504dccf6f0f43ff
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-10-01 12:55:05 -0400

    Job scripts updated for Trillium

---
+++ jobs/DRAC/compute_cluster_similarity_PONDSK_HBN.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=compute_cluster_similarity
+#SBATCH --nodes=2
+#SBATCH --time=1:00:00
+#SBATCH --chdir=/scratch/abeaucha/ClusteringAutism/main/
+
+# Activate virtual environment
+source activate_venv.sh
+
+# Execute pipeline
+compute_cluster_similarity.py \
+--pipeline-dir data/cross_species/v3/ \
+--species human human \
+--input-dirs data/human/derivatives/v3/ data/human/derivatives/v3/ \
+--input-params-ids 700 013 \
+--expr-dirs data/human/expression data/human/expression \
+--masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/human/registration/v3/reference_files/mask_0.8mm.mnc \
+--microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
+--gene-space vae-latent-space \
+--jacobians absolute relative \
+--execution local \
+--nproc 384
+
+deactivate
---
+++ jobs/DRAC/compute_cluster_similarity_PONDSK_MICe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=compute_cluster_similarity
+#SBATCH --nodes=2
+#SBATCH --time=1:00:00
+#SBATCH --chdir=/scratch/abeaucha/ClusteringAutism/main/
+
+# Activate virtual environment
+source activate_venv.sh
+
+# Execute pipeline
+compute_cluster_similarity.py \
+--pipeline-dir data/cross_species/v3/ \
+--species human mouse \
+--input-dirs data/human/derivatives/v3/ data/mouse/derivatives/v3/ \
+--input-params-ids 700 107 \
+--expr-dirs data/human/expression data/mouse/expression \
+--masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
+--microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
+--gene-space vae-latent-space \
+--jacobians absolute relative \
+--execution local \
+--nproc 384
+
+deactivate
