2eb181e82aa031124ccc04b8e4f1500d1c69b2ef
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-10-03 16:45:15 -0400

    Updated job for HBN-MICe permutation pipeline

--- jobs/DRAC/permute_cluster_similarity_HBN_MICe.sh
+++ jobs/DRAC/permute_cluster_similarity_HBN_MICe.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=permute_cluster_similarity
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=192
 #SBATCH --time=24:00:00
-#SBATCH --chdir=/hpf/largeprojects/MICe/abeauchamp/Paper_ClusteringAutism/main
-#SBATCH --output=logs/permute_cluster_similarity_v3_HBN_MICe_%j.out
-#SBATCH --dependency=afterok:14815989
+#SBATCH --chdir=/scratch/abeaucha/ClusteringAutism/main/
+#SBATCH --output=logs/permute_similarity_HBN_MICe_%j.out
 
 # Activate virtual environment
-source activate_venv_hpc.sh
-
-# Pipeline registry directory
-REGISTRY="permutations_registry_${SLURM_JOB_ID}"
+source activate_venv.sh
 
 # Execute pipeline
-# Maximum of 300 jobs allowed on HPF (??)
 permute_cluster_similarity.py \
 --pipeline-dir data/cross_species/v3/ \
---param-id 861 \
+--params-id 021 \
 --input-dirs data/human/derivatives/v3/ data/mouse/derivatives/v3/ \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---permutations-start 411 \
---permutations-n 10 \
+--permutations-start 1 \
+--permutations-n 100 \
 --off-diagonal 1 \
---execution slurm \
---registry-name $REGISTRY \
---registry-cleanup true \
---slurm-njobs 300 \
---slurm-mem 16G \
---slurm-time 8:00:00
-
-# --permutations-ids 95 98 \
+--execution local \
+--nproc 384
