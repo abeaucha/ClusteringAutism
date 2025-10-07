2d2c811b9c016db0264d0efe6ccdc4f90751a40e
Antoine Beauchamp <abeauchamp@hollandbloorview.ca>
2025-10-07 11:13:28 -0400

    Updated jobs

--- jobs/DRAC/permute_cluster_similarity_PONDSK_MICe.sh
+++ jobs/DRAC/permute_cluster_similarity_PONDSK_MICe.sh
@@ -18,8 +18,8 @@ permute_cluster_similarity.py \
 --expr-dirs data/human/expression data/mouse/expression \
 --masks data/human/registration/v3/reference_files/mask_0.8mm.mnc data/mouse/atlas/coronal_200um_coverage_bin0.8.mnc \
 --microarray-coords data/human/expression/v3/AHBA_microarray_coordinates_study.csv \
---permutations-start 129 \
---permutations-n 71 \
+--permutations-start 200 \
+--permutations-n 101 \
 --off-diagonal 1 \
 --execution local \
 --nproc 384
